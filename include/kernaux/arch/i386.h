13a23066228b5830d7f7ba1c529305e7d116a87f
Alex Kotov <kotovalexarian@gmail.com>
2020-11-27 16:00:34 +0500

    Add funcs to work with i386 cr* registers

--- include/kernaux/arch/i386.h
+++ include/kernaux/arch/i386.h
@@ -5,6 +5,13 @@
 extern "C" {
 #endif
 
+unsigned long kernaux_arch_i386_read_cr0();
+unsigned long kernaux_arch_i386_read_cr4();
+
+void kernaux_arch_i386_write_cr0(volatile unsigned long value);
+void kernaux_arch_i386_write_cr3(volatile unsigned long value);
+void kernaux_arch_i386_write_cr4(volatile unsigned long value);
+
 #ifdef __cplusplus
 }
 #endif
