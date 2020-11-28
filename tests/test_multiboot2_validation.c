96d9f1e65e7100fd7930f50e58cb9b432abf487e
Alex Kotov <kotovalexarian@gmail.com>
2020-11-29 03:51:27 +0500

    Validate that Multiboot 2 memory map tag entry size is mul of 8

--- tests/test_multiboot2_validation.c
+++ tests/test_multiboot2_validation.c
@@ -421,6 +421,44 @@ static const struct {
     },
 };
 
+static const struct KernAux_Multiboot2_Tag_MemoryMap
+tag_memory_map_with_empty_data_invalid_entry_size_not_mul8 = {
+    .base = {
+        .type = KERNAUX_MULTIBOOT2_TAGTYPE_MEMORY_MAP,
+        .size = 16,
+    },
+    .entry_size = 9,
+    .entry_version = 0,
+};
+
+static const struct {
+    struct KernAux_Multiboot2_Tag_MemoryMap tag;
+    unsigned char data[9 * 2];
+} tag_memory_map_with_some_small_data_items_invalid_entry_size_not_mul8 = {
+    .tag = {
+        .base = {
+            .type = KERNAUX_MULTIBOOT2_TAGTYPE_MEMORY_MAP,
+            .size = 16 + 9 * 2,
+        },
+        .entry_size = 9,
+        .entry_version = 123,
+    },
+};
+
+static const struct {
+    struct KernAux_Multiboot2_Tag_MemoryMap tag;
+    unsigned char data[63 * 2];
+} tag_memory_map_with_some_large_data_items_invalid_entry_size_not_mul8 = {
+    .tag = {
+        .base = {
+            .type = KERNAUX_MULTIBOOT2_TAGTYPE_MEMORY_MAP,
+            .size = 16 + 63 * 2,
+        },
+        .entry_size = 63,
+        .entry_version = 123,
+    },
+};
+
 /***************
  * Tag_VBEInfo *
  ***************/
@@ -900,6 +938,20 @@ int main()
         &tag_memory_map_with_some_large_data_items_invalid_size.tag.base
     ));
 
+    assert(!KernAux_Multiboot2_TagBase_is_valid(
+        &tag_memory_map_with_empty_data_invalid_entry_size_not_mul8.base
+    ));
+
+    assert(!KernAux_Multiboot2_TagBase_is_valid(
+        &tag_memory_map_with_some_small_data_items_invalid_entry_size_not_mul8
+            .tag.base
+    ));
+
+    assert(!KernAux_Multiboot2_TagBase_is_valid(
+        &tag_memory_map_with_some_large_data_items_invalid_entry_size_not_mul8
+            .tag.base
+    ));
+
     assert(KernAux_Multiboot2_TagBase_is_valid(
         &tag_vbe_info_valid.base
     ));
@@ -1036,6 +1088,20 @@ int main()
 
     assert(!KernAux_Multiboot2_Tag_MemoryMap_is_valid(
         &tag_memory_map_with_some_large_data_items_invalid_size.tag
+    ));
+
+    assert(!KernAux_Multiboot2_Tag_MemoryMap_is_valid(
+        &tag_memory_map_with_empty_data_invalid_entry_size_not_mul8
+    ));
+
+    assert(!KernAux_Multiboot2_Tag_MemoryMap_is_valid(
+        &tag_memory_map_with_some_small_data_items_invalid_entry_size_not_mul8
+            .tag
+    ));
+
+    assert(!KernAux_Multiboot2_Tag_MemoryMap_is_valid(
+        &tag_memory_map_with_some_large_data_items_invalid_entry_size_not_mul8
+            .tag
     ));
 
     // Tag_VBEInfo
