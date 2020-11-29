8b91aa227cb256d48ca25b80240906b899598704
Alex Kotov <kotovalexarian@gmail.com>
2020-11-30 04:58:13 +0500

    Start working on page frame allocator

--- include/kernaux/pfa.h
+++ include/kernaux/pfa.h
@@ -1,9 +1,55 @@
 #ifndef KERNAUX_INCLUDED_PFA
 #define KERNAUX_INCLUDED_PFA 1
 
+#define KERNAUX_PFA_ZONES_COUNT_MAX 10
+#define KERNAUX_PFA_ZONE_NAME_SIZE_MAX 256
+#define KERNAUX_PFA_ZONE_NAME_SLEN_MAX (KERNAUX_PFA_ZONE_NAME_SIZE_MAX - 1)
+#define KERNAUX_PFA_PAGE_SIZE_MIN (4 * 1024)
+#define KERNAUX_PFA_PAGE_SIZE_MAX (((unsigned long long)4) * 1024 * 1024 * 1024)
+#define KERNAUX_PFA_ZONE_PAGES_COUNT_MAX (1024 * 1024)
+#define KERNAUX_PFA_ZONE_PAGE_LIST_SIZE (KERNAUX_PFA_ZONE_PAGES_COUNT_MAX / 8)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+struct KernAux_PFA_Zone {
+    char name[KERNAUX_PFA_ZONE_NAME_SIZE_MAX];
+
+    unsigned long long start;
+    unsigned long long end;
+    unsigned long long size;
+
+    unsigned char pages[KERNAUX_PFA_ZONE_PAGE_LIST_SIZE];
+};
+
+struct KernAux_PFA {
+    unsigned char initialized;
+
+    unsigned long long page_size;
+
+    struct KernAux_PFA_Zone zones[KERNAUX_PFA_ZONES_COUNT_MAX];
+    unsigned int zones_count;
+};
+
+unsigned char KernAux_PFA_initialize_start(
+    struct KernAux_PFA *pfa,
+    unsigned long long page_size
+)
+__attribute__((nonnull));
+
+unsigned char KernAux_PFA_initialize_add_zone(
+    struct KernAux_PFA *pfa,
+    const char *name,
+    unsigned long long start,
+    unsigned long long end
+)
+__attribute__((nonnull));
+
+unsigned char KernAux_PFA_initialize_finish(
+    struct KernAux_PFA *pfa
+)
+__attribute__((nonnull));
 
 #ifdef __cplusplus
 }
--- src/pfa.c
+++ src/pfa.c
@@ -1,1 +1,48 @@
 #include <kernaux/pfa.h>
+
+unsigned char KernAux_PFA_initialize_start(
+    struct KernAux_PFA *const pfa,
+    const unsigned long long page_size
+) {
+    if (pfa->initialized) {
+        return 0;
+    }
+
+    if (page_size < KERNAUX_PFA_PAGE_SIZE_MIN) {
+        return 0;
+    }
+
+    if (page_size > KERNAUX_PFA_PAGE_SIZE_MAX) {
+        return 0;
+    }
+
+    for (unsigned long long i = 0; i < sizeof(*pfa); ++i) {
+        *((unsigned char*)pfa + i) = 0;
+    }
+
+    pfa->page_size = page_size;
+    pfa->zones_count = 0;
+}
+
+unsigned char KernAux_PFA_initialize_add_zone(
+    struct KernAux_PFA *const pfa,
+    const char *const name,
+    const unsigned long long start,
+    const unsigned long long end
+) {
+    if (pfa->initialized) {
+        return 0;
+    }
+
+    if (pfa->zones_count >= KERNAUX_PFA_ZONES_COUNT_MAX) {
+        return 0;
+    }
+}
+
+unsigned char KernAux_PFA_initialize_finish(
+    struct KernAux_PFA *const pfa
+) {
+    if (pfa->initialized) {
+        return 0;
+    }
+}
