0c0bde186fe727782084a87c10219aa213076449
Alex Kotov <kotovalexarian@gmail.com>
2020-11-30 05:52:29 +0500

    Add more tests for "kernaux_itoa"

--- tests/test_stdlib.c
+++ tests/test_stdlib.c
@@ -68,12 +68,25 @@ int main()
     {
         char buffer[10];
 
-        kernaux_itoa(495, buffer, 10);
+        kernaux_itoa(495, buffer, 'd');
 
         assert(buffer[0] == '4');
         assert(buffer[1] == '9');
         assert(buffer[2] == '5');
         assert(buffer[3] == '\0');
+    }
+
+    {
+        char buffer[10];
+
+        kernaux_itoa(-7012, buffer, 'd');
+
+        assert(buffer[0] == '-');
+        assert(buffer[1] == '7');
+        assert(buffer[2] == '0');
+        assert(buffer[3] == '1');
+        assert(buffer[4] == '2');
+        assert(buffer[5] == '\0');
     }
 
     return 0;
