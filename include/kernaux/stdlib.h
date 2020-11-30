edbd31e02d43a161e7d517d66106cbd7177e3fa1
Alex Kotov <kotovalexarian@gmail.com>
2020-11-30 05:00:47 +0500

    Add header <kernaux/stdlib.h>

---
+++ include/kernaux/stdlib.h
@@ -0,0 +1,12 @@
+#ifndef KERNAUX_INCLUDED_STDLIB
+#define KERNAUX_INCLUDED_STDLIB 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/stdlib.c
@@ -0,0 +1,1 @@
+#include <kernaux/stdlib.h>
