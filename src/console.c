ba254f41178d0a84abacbcd6b5501cc8241e587b
Alex Kotov <kotovalexarian@gmail.com>
2020-12-06 06:41:31 +0500

    Improve code style

--- src/console.c
+++ src/console.c
@@ -12,7 +12,8 @@ void kernaux_console_print(const char *const s)
     kernaux_console_write(s, kernaux_strlen(s));
 }
 
-void kernaux_console_putc(const char c __attribute__((unused))) {
+void kernaux_console_putc(const char c __attribute__((unused)))
+{
 #ifdef ARCH_X86
     kernaux_arch_x86_outportb(0x3F8, c);
 #endif
@@ -24,7 +25,8 @@ void kernaux_console_puts(const char *const s)
     kernaux_console_putc('\n');
 }
 
-void kernaux_console_write(const char *const data, const unsigned int size) {
+void kernaux_console_write(const char *const data, const unsigned int size)
+{
     for (unsigned int i = 0; i < size; i++) {
         kernaux_console_putc(data[i]);
     }
