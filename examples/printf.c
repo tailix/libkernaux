d3f6951dbe40eb04c44e406819ed4d07031bff9b
Alex Kotov <kotovalexarian@gmail.com>
2020-12-07 03:24:43 +0500

    Add example for <kernaux/printf.h>

---
+++ examples/printf.c
@@ -0,0 +1,26 @@
+#include <kernaux/printf.h>
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+
+static char buffer[BUFFER_SIZE];
+static unsigned int buffer_index;
+
+static void my_putchar(const char chr)
+{
+    if (buffer_index >= BUFFER_SIZE) abort();
+    buffer[buffer_index++] = chr;
+}
+
+int main()
+{
+    memset(buffer, '\0', sizeof(buffer));
+    buffer_index = 0;
+    kernaux_printf(my_putchar, "Hello, World!");
+    assert(strcmp(buffer, "Hello, World!") == 0);
+
+    return 0;
+}
