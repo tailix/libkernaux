566fb46a9480bc2088d1ffc87fd831ccbdaa4e8e
Alex Kotov <kotovalexarian@gmail.com>
2021-12-14 02:54:35 +0500

    Add header <kernaux.h>

---
+++ include/kernaux.h
@@ -0,0 +1,8 @@
+#include <kernaux/cmdline.h>
+#include <kernaux/console.h>
+#include <kernaux/elf.h>
+#include <kernaux/multiboot2.h>
+#include <kernaux/pfa.h>
+#include <kernaux/printf.h>
+#include <kernaux/stdlib.h>
+#include <kernaux/units.h>
