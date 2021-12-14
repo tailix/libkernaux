8d00ffb6fd13df0f4147c9580f5fd37a9affdf51
Alex Kotov <kotovalexarian@gmail.com>
2021-12-15 03:12:45 +0500

    Fix CI

--- src/assert.c
+++ src/assert.c
@@ -9,8 +9,17 @@
 void (*kernaux_assert_cb)(const char *file, int line, const char *str) = NULL;
 
 void kernaux_assert_do(
+#ifndef ENABLE_ASSERT
+    __attribute__((unused))
+#endif
     const char *const file,
+#ifndef ENABLE_ASSERT
+    __attribute__((unused))
+#endif
     const int line,
+#ifndef ENABLE_ASSERT
+    __attribute__((unused))
+#endif
     const char *const str
 ) {
 #ifdef ENABLE_ASSERT
