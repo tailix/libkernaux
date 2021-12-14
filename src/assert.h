e7c3679594a04948f812e5b42a7429579947ee70
Alex Kotov <kotovalexarian@gmail.com>
2021-12-15 04:55:26 +0500

    Remove unnecessary parentheses

--- src/assert.h
+++ src/assert.h
@@ -1,10 +1,10 @@
 #include <kernaux/assert.h>
 
 #ifdef ENABLE_ASSERT
-#define assert(cond) kernaux_assert((cond))
+#define assert(cond) kernaux_assert(cond)
 #else
 #define assert(cond) ((void)sizeof((cond)))
 #endif
 
-#define assert_return(cond)      { assert((cond)); if (!(cond)) return; }
-#define assert_retval(cond, val) { assert((cond)); if (!(cond)) return (val); }
+#define assert_return(cond)      { assert(cond); if (!(cond)) return; }
+#define assert_retval(cond, val) { assert(cond); if (!(cond)) return (val); }
