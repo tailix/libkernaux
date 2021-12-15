1e8ae27a60e78937320b842ad88f11c356d1f3c7
Alex Kotov <kotovalexarian@gmail.com>
2021-12-15 14:51:50 +0500

    Add autoconf options "--enable-libc-*"

--- include/kernaux/libc.h
+++ include/kernaux/libc.h
@@ -8,8 +8,8 @@ extern "C" {
 #include <stddef.h>
 
 void *memset(void *s, int c, size_t n);
-size_t strlen(const char *s);
 char *strcpy(char *dest, const char *src);
+size_t strlen(const char *s);
 
 #ifdef __cplusplus
 }
