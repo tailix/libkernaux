fbfd178f590f402c2ca95d164e3a6a83d32c614b
Alex Kotov <kotovalexarian@gmail.com>
2021-12-19 08:40:23 +0500

    Add test "arch_i386_paging"

---
+++ tests/test_arch_i386_paging.c
@@ -0,0 +1,32 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <kernaux/arch/i386.h>
+
+// TODO: There are some static asserts just for example.
+
+__attribute__((unused))
+static const int assert0[
+    sizeof(struct KernAux_Arch_I386_PDE) == sizeof(uint32_t) ? 1 : -1
+];
+
+__attribute__((unused))
+static const int assert1[
+    sizeof(struct KernAux_Arch_I386_PTE) == sizeof(uint32_t) ? 1 : -1
+];
+
+__attribute__((unused))
+static const int assert1[
+    sizeof(struct KernAux_Arch_I386_PageDir) == 1024 * sizeof(uint32_t) ? 1 : -1
+];
+
+__attribute__((unused))
+static const int assert1[
+    sizeof(struct KernAux_Arch_I386_PageTable) == 1024 * sizeof(uint32_t) ? 1 : -1
+];
+
+int main()
+{
+    return 0;
+}
