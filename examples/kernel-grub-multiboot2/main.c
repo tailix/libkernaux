163e775fc8e7211312cd257ba297dc2defd13371
Alex Kotov <kotovalexarian@gmail.com>
2022-01-09 08:30:36 +0500

    Use printf

--- examples/kernel-grub-multiboot2/main.c
+++ examples/kernel-grub-multiboot2/main.c
@@ -1,12 +1,15 @@
+#include <stdarg.h>
 #include <stdint.h>
 
 #include <kernaux/asm/i386.h>
 #include <kernaux/drivers/console.h>
 #include <kernaux/multiboot2.h>
+#include <kernaux/printf.h>
 
 void halt();
 
 static void panic(const char *str);
+static void printf(const char *format, ...);
 
 void main(
     const uint32_t multiboot2_magic,
@@ -16,7 +19,7 @@ void main(
         panic("Multiboot 2 magic number is invalid");
     }
 
-    KernAux_Multiboot2_print(multiboot2_info, kernaux_console_printf);
+    KernAux_Multiboot2_print(multiboot2_info, printf);
 
     if (!KernAux_Multiboot2_is_valid(multiboot2_info)) {
         panic("Multiboot 2 info is invalid");
@@ -30,6 +33,15 @@ void halt()
 
 void panic(const char *const str)
 {
-    kernaux_console_printf("panic: %s\n", str);
+    printf("panic: %s\n", str);
     kernaux_asm_i386_hang();
+}
+
+void printf(const char *const format, ...)
+{
+    va_list va;
+
+    va_start(va, format);
+    kernaux_printf_va(kernaux_console_putc, format, va);
+    va_end(va);
 }
