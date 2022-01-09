7bd6ee7d2aee1e9f893ed76fd7d73a43ff19a95d
Alex Kotov <kotovalexarian@gmail.com>
2022-01-09 08:06:03 +0500

    Add Multiboot 2 i386 kernel example

---
+++ examples/multiboot2-i386-kernel/main.c
@@ -0,0 +1,35 @@
+#include <stdint.h>
+
+#include <kernaux/asm/i386.h>
+#include <kernaux/drivers/console.h>
+#include <kernaux/multiboot2.h>
+
+void halt();
+
+static void panic(const char *str);
+
+void main(
+    const uint32_t multiboot2_magic,
+    const struct KernAux_Multiboot2 *const multiboot2_info
+) {
+    if (multiboot2_magic != KERNAUX_MULTIBOOT2_MAGIC) {
+        panic("Multiboot 2 magic number is invalid");
+    }
+
+    KernAux_Multiboot2_print(multiboot2_info, kernaux_console_printf);
+
+    if (!KernAux_Multiboot2_is_valid(multiboot2_info)) {
+        panic("Multiboot 2 info is invalid");
+    }
+}
+
+void halt()
+{
+    panic("Kernel main function returned");
+}
+
+void panic(const char *const str)
+{
+    kernaux_console_printf("panic: %s\n", str);
+    kernaux_asm_i386_hang();
+}
