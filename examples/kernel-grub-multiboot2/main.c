9a02e57a75368e6d37b3015fcf40c3242f32d9c5
Alex Kotov <kotovalexarian@gmail.com>
2022-01-10 06:30:40 +0500

    Remove unnecessary example code

--- examples/kernel-grub-multiboot2/main.c
+++ examples/kernel-grub-multiboot2/main.c
@@ -4,12 +4,10 @@
 #include <kernaux/asm/i386.h>
 #include <kernaux/drivers/console.h>
 #include <kernaux/multiboot2.h>
-#include <kernaux/printf.h>
 
 void halt();
 
 static void panic(const char *str);
-static void printf(const char *format, ...);
 
 void main(
     const uint32_t multiboot2_magic,
@@ -23,7 +21,7 @@ void main(
         panic("Multiboot 2 info is invalid");
     }
 
-    KernAux_Multiboot2_print(multiboot2_info, printf);
+    KernAux_Multiboot2_print(multiboot2_info, kernaux_console_printf);
 }
 
 void halt()
@@ -33,15 +31,6 @@ void halt()
 
 void panic(const char *const str)
 {
-    printf("panic: %s\n", str);
+    kernaux_console_printf("panic: %s\n", str);
     kernaux_asm_i386_hang();
-}
-
-void printf(const char *const format, ...)
-{
-    va_list va;
-
-    va_start(va, format);
-    kernaux_printf_va(kernaux_console_putc, format, va);
-    va_end(va);
 }
