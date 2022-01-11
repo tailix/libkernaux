a793875c405b5ac4b5e59c8902ff918a3b7fc265
Alex Kotov <kotovalexarian@gmail.com>
2022-01-11 11:28:36 +0500

    Do not use "unsigned *" in packed structs

--- include/kernaux/arch/i386.h
+++ include/kernaux/arch/i386.h
@@ -52,21 +52,21 @@ extern "C" {
 // Global descriptor table entry
 // TODO: validate this according to spec
 struct KernAux_Arch_I386_GDTE {
-    unsigned int limit_low              : 16;
-    unsigned int base_low               : 24;
-    unsigned int accessed               : 1;
-    unsigned int read_write             : 1;
-    unsigned int conforming_expand_down : 1;
-    unsigned int code                   : 1;
-    unsigned int always_1               : 1;
-    unsigned int DPL                    : 2;
-    unsigned int present                : 1;
-    unsigned int limit_high             : 4;
-    unsigned int available              : 1;
-    unsigned int always_0               : 1;
-    unsigned int big                    : 1;
-    unsigned int gran                   : 1;
-    unsigned int base_high              : 8;
+    unsigned limit_low              : 16;
+    unsigned base_low               : 24;
+    unsigned accessed               : 1;
+    unsigned read_write             : 1;
+    unsigned conforming_expand_down : 1;
+    unsigned code                   : 1;
+    unsigned always_1               : 1;
+    unsigned DPL                    : 2;
+    unsigned present                : 1;
+    unsigned limit_high             : 4;
+    unsigned available              : 1;
+    unsigned always_0               : 1;
+    unsigned big                    : 1;
+    unsigned gran                   : 1;
+    unsigned base_high              : 8;
 }
 __attribute__((packed));
 
