8af023c4786dd5cdb8e74f0eb1feda049133ee81
Alex Kotov <kotovalexarian@gmail.com>
2022-01-11 11:26:33 +0500

    Use "uint8_t" instead of "unsigned char"

--- tests/test_elf.c
+++ tests/test_elf.c
@@ -6,6 +6,7 @@
 
 #include <assert.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 
 #define BUFFER_SIZE (1024 * 1024)
@@ -17,8 +18,8 @@ int main(int argc, char **argv)
     FILE *const fd = fopen(argv[0], "r");
     assert(fd);
 
-    unsigned char buffer[BUFFER_SIZE];
-    const size_t size = fread(buffer, sizeof(unsigned char), BUFFER_SIZE, fd);
+    uint8_t buffer[BUFFER_SIZE];
+    const size_t size = fread(buffer, sizeof(uint8_t), BUFFER_SIZE, fd);
     assert(size > 0);
 
     assert(KernAux_ELF_Header_is_valid((struct KernAux_ELF_Header*)buffer));
