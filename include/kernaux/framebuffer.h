b579ad0d8ea4723c8b1bb0f53ed30a615a81e88c
Ashu Sharma <ashusharma.sigmadev@gmail.com>
2022-01-12 02:42:39 +0530

    putpixel function & framebuffer struct

--- include/kernaux/framebuffer.h
+++ include/kernaux/framebuffer.h
@@ -1,6 +1,8 @@
 #ifndef KERNAUX_INCLUDED_FRAMEBUFFER
 #define KERNAUX_INCLUDED_FRAMEBUFFER
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -8,12 +10,24 @@ extern "C" {
 typedef struct
 {
     
-    int framewidth;
-    int frameheight;
-    
-
+    uint32_t    frame_width;
+    uint32_t    frame_height;
+    uint32_t    frame_pitch;
+    uint32_t    frame_depth;
+    uintptr_t   buffer_addr;
+    uint8_t     redmask_size;
+    uint8_t     redmask_shift;
+    uint8_t     greenmask_size;
+    uint8_t     greenmask_shift;
+    uint8_t     bluemask_size;
+    uint8_t     bluemask_shift;
 } framebuffer_t;
 
+
+framebuffer_t framebuffer(uint32_t width, uint32_t height, uint32_t pitch, uint32_t depth, uintptr_t buf_addr);
+framebuffer_t framebuffer(uint32_t width, uint32_t height, uint32_t pitch, uint32_t depth, uintptr_t buf_addr, uint8_t redmask_sz, uint8_t redmask_shift, uint8_t greenmask_sz, uint8_t greenmask_shift, uint8_t bluemask_sz, uint8_t bluemask_shift);
+
+void putpixel(framebuffer_t* _framebuffer, int x, int y, uint32_t hex);
 
 #ifdef __cplusplus
 }
--- src/framebuffer.c
+++ src/framebuffer.c
@@ -4,5 +4,66 @@
 
 #include <kernaux/framebuffer.h>
 
-__attribute__((unused))
-static const int foobar = 0;
+framebuffer_t framebuffer(uint32_t width, uint32_t height, uint32_t pitch, uint32_t depth, uintptr_t buf_addr)
+{
+    return (framebuffer_t){
+        .frame_width=width,
+        .frame_height=height,
+        .frame_pitch=pitch,
+        .frame_depth=depth,
+        .buffer_addr=buf_addr,
+        .redmask_size=0,
+        .redmask_shift=0,
+        .greenmask_size=0,
+        .greenmask_shift=0,
+        .bluemask_size=0,
+        .bluemask_shift=0
+    };
+}
+
+framebuffer_t framebuffer(uint32_t width, uint32_t height, uint32_t pitch, uint32_t depth, uintptr_t buf_addr, uint8_t redmask_sz, uint8_t redmask_shift, uint8_t greenmask_sz, uint8_t greenmask_shift, uint8_t bluemask_sz, uint8_t bluemask_shift)
+{
+    return (framebuffer_t){
+        .frame_width=width,
+        .frame_height=height,
+        .frame_pitch=pitch,
+        .frame_depth=depth,
+        .buffer_addr=buf_addr,
+        .redmask_size=redmask_sz,
+        .redmask_shift=redmask_shift,
+        .greenmask_size=greenmask_sz,
+        .greenmask_shift=greenmask_shift,
+        .bluemask_size=bluemask_sz,
+        .bluemask_shift=bluemask_shift
+    };
+}
+
+
+void putpixel(framebuffer_t* _framebuffer, int x, int y, uint32_t hex)
+{
+    switch (_framebuffer->frame_depth)
+    {
+        case 32:
+        {
+            uint32_t *spix = (uint32_t*)_framebuffer->buffer_addr;
+            size_t vr = (y * _framebuffer->frame_pitch) / 4;
+            spix[vr+x] = hex;
+            break;
+        }
+        case 16:
+        {
+            uint16_t *spix = (uint16_t*)_framebuffer->buffer_addr;
+            size_t vr = (y * _framebuffer->frame_pitch) / 2;
+            spix[vr+x] = hex;
+            break;
+        }
+        default:
+        {
+            /* 8 Bit */
+            uint8_t *spix = (uint8_t*)_framebuffer->buffer_addr;
+            size_t vr = (y * _framebuffer->frame_pitch);
+            spix[vr+x] = hex;
+            break;
+        }
+    }
+}
