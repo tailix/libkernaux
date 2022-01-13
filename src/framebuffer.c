2b8a2b312c36de3b25054d0ce3c54d78a8e515d7
Alex Kotov <kotovalexarian@gmail.com>
2022-01-13 10:56:01 +0500

    Fix compilation errors

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -7,12 +7,12 @@
 
 void KernAux_Framebuffer_init_default(KernAux_Framebuffer* _framebuffer, uint32_t width, uint32_t height, uint32_t pitch, uint32_t depth, uintptr_t buf_addr, uint8_t frame_type)
 {
-    KERNAUX_NOTNULL_RETURL(_framebuffer);
+    KERNAUX_NOTNULL_RETURN(_framebuffer);
     KERNAUX_ASSERT_RETURN(width > 0);
     KERNAUX_ASSERT_RETURN(height > 0);
     KERNAUX_ASSERT_RETURN(pitch > 0);
     KERNAUX_ASSERT_RETURN(depth > 0);
-    KERNAUX_ASSERT(buf_addr != NULL);
+    KERNAUX_NOTNULL_RETURN(buf_addr);
 
     _framebuffer->frame_width = width;
     _framebuffer->frame_height = height;
@@ -30,12 +30,12 @@ void KernAux_Framebuffer_init_default(KernAux_Framebuffer* _framebuffer, uint32_
 
 void KernAux_Framebuffer_init(KernAux_Framebuffer* _framebuffer, uint32_t width, uint32_t height, uint32_t pitch, uint32_t depth, uintptr_t buf_addr, uint8_t frame_type, uint8_t redmask_sz, uint8_t redmask_shift, uint8_t greenmask_sz, uint8_t greenmask_shift, uint8_t bluemask_sz, uint8_t bluemask_shift)
 {
-    KERNAUX_NOTNULL_RETURL(_framebuffer);
+    KERNAUX_NOTNULL_RETURN(_framebuffer);
     KERNAUX_ASSERT_RETURN(width > 0);
     KERNAUX_ASSERT_RETURN(height > 0);
     KERNAUX_ASSERT_RETURN(pitch > 0);
     KERNAUX_ASSERT_RETURN(depth > 0);
-    KERNAUX_ASSERT(buf_addr != NULL);
+    KERNAUX_NOTNULL_RETURN(buf_addr);
 
     _framebuffer->frame_width = width;
     _framebuffer->frame_height = height;
@@ -53,6 +53,7 @@ void KernAux_Framebuffer_init(KernAux_Framebuffer* _framebuffer, uint32_t width,
 
 void KernAux_Framebuffer_putpixel_rgb(KernAux_Framebuffer *_framebuffer, int x, int y, uint32_t hex)
 {
+    KERNAUX_NOTNULL_RETURN(_framebuffer);
     KERNAUX_ASSERT_RETURN(_framebuffer->frame_type == FRAME_TYPE_RGB);
     KERNAUX_ASSERT_RETURN(x <= _framebuffer->frame_width);
     KERNAUX_ASSERT_RETURN(y <= _framebuffer->frame_height);
@@ -85,9 +86,9 @@ void KernAux_Framebuffer_putpixel_rgb(KernAux_Framebuffer *_framebuffer, int x, 
     }
 }
 
-
 void KernAux_Framebuffer_putpixel_ega(KernAux_Framebuffer* _framebuffer, int x, int y, enum kernAux_vga_color_palatte color)
 {
+    KERNAUX_NOTNULL_RETURN(_framebuffer);
     KERNAUX_ASSERT_RETURN(_framebuffer->frame_type == FRAME_TYPE_EGA);
     KERNAUX_ASSERT_RETURN(x <= _framebuffer->frame_width);
     KERNAUX_ASSERT_RETURN(y <= _framebuffer->frame_height);
@@ -95,21 +96,25 @@ void KernAux_Framebuffer_putpixel_ega(KernAux_Framebuffer* _framebuffer, int x, 
     switch (_framebuffer->frame_depth)
     {
         case 16:
+        {
             uint16_t *spix = (uint16_t *)_framebuffer->buffer_addr;
             size_t vr = (y * _framebuffer->frame_pitch) / 2;
             spix[vr + x] = color;
             break;
+        }
         default:
+        {
             uint8_t *spix = (uint8_t *)_framebuffer->buffer_addr;
             size_t vr = (y * _framebuffer->frame_pitch);
             spix[vr + x] = color;
             break;
+        }
     }
 }
 
-
 void KernAux_Framebuffer_fillarea_rgb(KernAux_Framebuffer *_framebuffer, int x0, int y0, int x1, int y1, uint32_t hex)
 {
+    KERNAUX_NOTNULL_RETURN(_framebuffer);
     KERNAUX_ASSERT_RETURN(_framebuffer->frame_type == FRAME_TYPE_RGB);
     KERNAUX_ASSERT_RETURN((y0+y1) <= _framebuffer->frame_height);
     KERNAUX_ASSERT_RETURN((x0+x1) <= _framebuffer->frame_width);
@@ -125,6 +130,7 @@ void KernAux_Framebuffer_fillarea_rgb(KernAux_Framebuffer *_framebuffer, int x0,
 
 void KernAux_Framebuffer_fillarea_ega(KernAux_Framebuffer* _framebuffer, int x0, int y0, int x1, int y1, enum kernAux_vga_color_palatte color)
 {
+    KERNAUX_NOTNULL_RETURN(_framebuffer);
     KERNAUX_ASSERT_RETURN(_framebuffer->frame_type == FRAME_TYPE_EGA);
     KERNAUX_ASSERT_RETURN((y0+y1) <= _framebuffer->frame_height);
     KERNAUX_ASSERT_RETURN((x0+x1) <= _framebuffer->frame_width);
