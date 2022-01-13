45fae15b9808bbe92bd35d52e2f3d04de5fb5c93
Alex Kotov <kotovalexarian@gmail.com>
2022-01-13 11:06:24 +0500

    Fix code style

--- include/kernaux/framebuffer.h
+++ include/kernaux/framebuffer.h
@@ -33,30 +33,30 @@ enum KernAux_Framebuffer_VGAColorPalette {
 };
 
 typedef struct KernAux_Framebuffer {
-    uint32_t    frame_width;
-    uint32_t    frame_height;
-    uint32_t    frame_pitch;
-    uint32_t    frame_depth;
-    uintptr_t   buffer_addr;
-    uint8_t     frame_type;
-    uint8_t     redmask_size;
-    uint8_t     redmask_shift;
-    uint8_t     greenmask_size;
-    uint8_t     greenmask_shift;
-    uint8_t     bluemask_size;
-    uint8_t     bluemask_shift;
+    uint32_t  frame_width;
+    uint32_t  frame_height;
+    uint32_t  frame_pitch;
+    uint32_t  frame_depth;
+    uintptr_t buffer_addr;
+    uint8_t   frame_type;
+    uint8_t   redmask_size;
+    uint8_t   redmask_shift;
+    uint8_t   greenmask_size;
+    uint8_t   greenmask_shift;
+    uint8_t   bluemask_size;
+    uint8_t   bluemask_shift;
 } *KernAux_Framebuffer;
 
 
-void KernAux_Framebuffer_init_default(KernAux_Framebuffer _framebuffer, uint32_t width, uint32_t height, uint32_t pitch, uint32_t depth, uintptr_t buf_addr, uint8_t frame_type);
+void KernAux_Framebuffer_init_default(KernAux_Framebuffer framebuffer, uint32_t width, uint32_t height, uint32_t pitch, uint32_t depth, uintptr_t buf_addr, uint8_t frame_type);
 void KernAux_Framebuffer_init(KernAux_Framebuffer _framebuffer, uint32_t width, uint32_t height, uint32_t pitch, uint32_t depth, uintptr_t buf_addr, uint8_t frame_type, uint8_t redmask_sz, uint8_t redmask_shift, uint8_t greenmask_sz, uint8_t greenmask_shift, uint8_t bluemask_sz, uint8_t bluemask_shift);
 
-void KernAux_Framebuffer_putpixel_rgb(KernAux_Framebuffer _framebuffer, int x, int y, uint32_t hex);
+void KernAux_Framebuffer_putpixel_rgb(KernAux_Framebuffer framebuffer, int x, int y, uint32_t hex);
 
-void KernAux_Framebuffer_putpixel_ega(KernAux_Framebuffer _framebuffer, int x, int y, enum KernAux_Framebuffer_VGAColorPalette color);
+void KernAux_Framebuffer_putpixel_ega(KernAux_Framebuffer framebuffer, int x, int y, enum KernAux_Framebuffer_VGAColorPalette color);
 
-void KernAux_Framebuffer_fillarea_rgb(KernAux_Framebuffer _framebuffer, int x0, int y0, int x1, int y1, uint32_t hex);
-void KernAux_Framebuffer_fillarea_ega(KernAux_Framebuffer _framebuffer, int x0, int y0, int x1, int y1, enum KernAux_Framebuffer_VGAColorPalette color);
+void KernAux_Framebuffer_fillarea_rgb(KernAux_Framebuffer framebuffer, int x0, int y0, int x1, int y1, uint32_t hex);
+void KernAux_Framebuffer_fillarea_ega(KernAux_Framebuffer framebuffer, int x0, int y0, int x1, int y1, enum KernAux_Framebuffer_VGAColorPalette color);
 
 #ifdef __cplusplus
 }
