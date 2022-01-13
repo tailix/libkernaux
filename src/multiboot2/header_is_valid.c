4836405fb237d07d70e24939f1d2ee40134d7a64
Alex Kotov <kotovalexarian@gmail.com>
2022-01-13 19:23:24 +0500

    Write code

--- src/multiboot2/header_is_valid.c
+++ src/multiboot2/header_is_valid.c
@@ -12,3 +12,87 @@ bool KernAux_Multiboot2_Header_is_valid(
     // TODO: write this
     return false;
 }
+
+bool KernAux_Multiboot2_HTagBase_is_valid(
+    const struct KernAux_Multiboot2_HTagBase *tag_base
+) {
+    // TODO: write this
+    return false;
+}
+
+bool KernAux_Multiboot2_HTag_None_is_valid(
+    const struct KernAux_Multiboot2_HTag_None *tag
+) {
+    // TODO: write this
+    return false;
+}
+
+bool KernAux_Multiboot2_HTag_InfoReq_is_valid(
+    const struct KernAux_Multiboot2_HTag_InfoReq *tag
+) {
+    // TODO: write this
+    return false;
+}
+
+bool KernAux_Multiboot2_HTag_Addr_is_valid(
+    const struct KernAux_Multiboot2_HTag_Addr *tag
+) {
+    // TODO: write this
+    return false;
+}
+
+bool KernAux_Multiboot2_HTag_EntryAddr_is_valid(
+    const struct KernAux_Multiboot2_HTag_EntryAddr *tag
+) {
+    // TODO: write this
+    return false;
+}
+
+bool KernAux_Multiboot2_HTag_Flags_is_valid(
+    const struct KernAux_Multiboot2_HTag_Flags *tag
+) {
+    // TODO: write this
+    return false;
+}
+
+bool KernAux_Multiboot2_HTag_Framebuffer_is_valid(
+    const struct KernAux_Multiboot2_HTag_Framebuffer *tag
+) {
+    // TODO: write this
+    return false;
+}
+
+bool KernAux_Multiboot2_HTag_ModuleAlign_is_valid(
+    const struct KernAux_Multiboot2_HTag_ModuleAlign *tag
+) {
+    // TODO: write this
+    return false;
+}
+
+bool KernAux_Multiboot2_HTag_EFIBootServices_is_valid(
+    const struct KernAux_Multiboot2_HTag_EFIBootServices *tag
+) {
+    // TODO: write this
+    return false;
+}
+
+bool KernAux_Multiboot2_HTag_EFII386EntryAddr_is_valid(
+    const struct KernAux_Multiboot2_HTag_EFII386EntryAddr *tag
+) {
+    // TODO: write this
+    return false;
+}
+
+bool KernAux_Multiboot2_HTag_EFIAmd64EntryAddr_is_valid(
+    const struct KernAux_Multiboot2_HTag_EFIAmd64EntryAddr *tag
+) {
+    // TODO: write this
+    return false;
+}
+
+bool KernAux_Multiboot2_HTag_RelocatableHeader_is_valid(
+    const struct KernAux_Multiboot2_HTag_RelocatableHeader *tag
+) {
+    // TODO: write this
+    return false;
+}
