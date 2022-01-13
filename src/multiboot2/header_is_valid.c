2c3714b99c7df3d75ae71d5d23d96e542869f833
Alex Kotov <kotovalexarian@gmail.com>
2022-01-13 19:32:19 +0500

    Write code

--- src/multiboot2/header_is_valid.c
+++ src/multiboot2/header_is_valid.c
@@ -5,12 +5,56 @@
 #include <kernaux/multiboot2.h>
 
 #include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 bool KernAux_Multiboot2_Header_is_valid(
     const struct KernAux_Multiboot2_Header *const multiboot2_header
 ) {
-    // TODO: write this
-    return false;
+    if (multiboot2_header->total_size <
+        sizeof(struct KernAux_Multiboot2_Header) +
+        sizeof(struct KernAux_Multiboot2_HTag_None))
+    {
+        return false;
+    }
+
+    const struct KernAux_Multiboot2_HTagBase *tag_base =
+        (struct KernAux_Multiboot2_HTagBase*)
+        KERNAUX_MULTIBOOT2_DATA(multiboot2_header);
+
+    const struct KernAux_Multiboot2_HTagBase *none_tag_base = NULL;
+
+    while (tag_base <
+           (struct KernAux_Multiboot2_HTagBase*)
+           ((uint8_t*)multiboot2_header + multiboot2_header->total_size))
+    {
+        if (!KernAux_Multiboot2_HTagBase_is_valid(tag_base)) return false;
+
+        if (tag_base->type == KERNAUX_MULTIBOOT2_HTAG_NONE &&
+            none_tag_base == NULL
+        ) {
+            none_tag_base = tag_base;
+        }
+
+        tag_base = KERNAUX_MULTIBOOT2_HTAG_NEXT(tag_base);
+    }
+
+    if (tag_base !=
+        (struct KernAux_Multiboot2_HTagBase*)
+        ((uint8_t*)multiboot2_header + multiboot2_header->total_size))
+    {
+        return false;
+    }
+
+    if (none_tag_base !=
+        (struct KernAux_Multiboot2_HTagBase*)
+        ((uint8_t*)tag_base -
+         sizeof(struct KernAux_Multiboot2_HTag_None)))
+    {
+        return false;
+    }
+
+    return true;
 }
 
 bool KernAux_Multiboot2_HTagBase_is_valid(
