00345258e9355de2bef8ed1164c28f505c868e0a
Alex Kotov <kotovalexarian@gmail.com>
2022-01-15 15:09:45 +0500

    Add arch "riscv64"

---
+++ include/kernaux/asm/riscv64.h
@@ -0,0 +1,14 @@
+#ifndef KERNAUX_INCLUDED_ASM_RISCV64
+#define KERNAUX_INCLUDED_ASM_RISCV64
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <kernaux/arch/riscv64.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
