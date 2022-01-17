f6b09497ee07168c1bc65e9d19b086eed8aa8667
Alex Kotov <kotovalexarian@gmail.com>
2022-01-17 23:06:32 +0500

    Move root to separate directory

--- vendor/cross/build/i386/build.sh
+++ vendor/cross/build/i386/build.sh
@@ -6,16 +6,18 @@ REPO="$(realpath "$(dirname "$(realpath "$0")")/../..")"
 
 ARCH='i386'
 TARGET="$ARCH-elf"
-PATH="$REPO/bin:$PATH"
+
+ROOT="$REPO/root"
+PATH="$ROOT/bin:$PATH"
 
 if [ -z "$J" ]; then J='1'; fi
 
 cd "$REPO/build/$ARCH/binutils"
-"$REPO/src/binutils-2.37/configure" --target="$TARGET" --prefix="$REPO" --with-sysroot --disable-nls --disable-werror
+"$REPO/src/binutils-2.37/configure" --target="$TARGET" --prefix="$ROOT" --with-sysroot --disable-nls --disable-werror
 make -j"$J"
 make install
 
 cd "$REPO/build/$ARCH/gcc"
-"$REPO/src/gcc-11.2.0/configure" --target="$TARGET" --prefix="$REPO" --disable-nls --enable-languages=c,c++ --without-headers
+"$REPO/src/gcc-11.2.0/configure" --target="$TARGET" --prefix="$ROOT" --disable-nls --enable-languages=c,c++ --without-headers
 make -j"$J" all-gcc all-target-libgcc
 make install-gcc install-target-libgcc
--- vendor/cross/build/x86_64/build.sh
+++ vendor/cross/build/x86_64/build.sh
@@ -6,16 +6,18 @@ REPO="$(realpath "$(dirname "$(realpath "$0")")/../..")"
 
 ARCH='x86_64'
 TARGET="$ARCH-elf"
-PATH="$REPO/bin:$PATH"
+
+ROOT="$REPO/root"
+PATH="$ROOT/bin:$PATH"
 
 if [ -z "$J" ]; then J='1'; fi
 
 cd "$REPO/build/$ARCH/binutils"
-"$REPO/binutils-2.37/configure" --target="$TARGET" --prefix="$REPO" --with-sysroot --disable-nls --disable-werror
+"$REPO/binutils-2.37/configure" --target="$TARGET" --prefix="$ROOT" --with-sysroot --disable-nls --disable-werror
 make -j"$J"
 make install
 
 cd "$REPO/build/$ARCH/gcc"
-"$REPO/gcc-11.2.0/configure" --target="$TARGET" --prefix="$REPO" --disable-nls --enable-languages=c,c++ --without-headers
+"$REPO/gcc-11.2.0/configure" --target="$TARGET" --prefix="$ROOT" --disable-nls --enable-languages=c,c++ --without-headers
 make -j"$J" all-gcc all-target-libgcc
 make install-gcc install-target-libgcc
