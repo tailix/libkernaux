5b5f67355a0561dc776a47a7a63c76c9b56a7c6d
Alex Kotov <kotovalexarian@gmail.com>
2022-01-17 22:51:35 +0500

    Improve cross-compiler directory structure

--- vendor/cross/fetch.sh
+++ vendor/cross/fetch.sh
@@ -6,8 +6,10 @@ REPO="$(realpath "$(dirname "$(realpath "$0")")")"
 
 cd "$REPO"
 
-wget -nc -i 'wget.txt'
+SRC="$REPO/src"
+
+wget -nc -i 'wget.txt' -P "$SRC"
 sha256sum -c 'sha256sums.txt'
 
-tar -xf 'binutils-2.37.tar.xz'
-tar -xf 'gcc-11.2.0.tar.xz'
+tar -xf "$SRC/binutils-2.37.tar.xz" -C "$SRC"
+tar -xf "$SRC/gcc-11.2.0.tar.xz"    -C "$SRC"
