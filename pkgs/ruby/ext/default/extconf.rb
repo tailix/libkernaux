6980fcd42bdf4773b07cfed3a64f5ee7e09923a4
Alex Kotov <kotovalexarian@gmail.com>
2022-01-18 22:24:50 +0500

    Add Ruby methods KernAux.utoa10, .itoa10

--- pkgs/ruby/ext/default/extconf.rb
+++ pkgs/ruby/ext/default/extconf.rb
@@ -2,4 +2,9 @@
 
 require 'mkmf'
 
+have_library 'kernaux'
+
+have_func 'kernaux_utoa10'
+have_func 'kernaux_itoa10'
+
 create_makefile 'kernaux/default'
--- pkgs/ruby/spec/lib/kernaux_spec.rb
+++ pkgs/ruby/spec/lib/kernaux_spec.rb
@@ -8,4 +8,102 @@ RSpec.describe KernAux do
       expect(described_class::VERSION).to match(/\A\d+\.\d+\.\d+\z/)
     end
   end
+
+  describe '.utoa10' do
+    subject(:utoa10) { described_class.utoa10 number }
+
+    let(:number) { rand 0..(2**64 - 1) }
+
+    it { is_expected.to be_instance_of String }
+    it { is_expected.to be_frozen }
+    it { is_expected.to eq number.to_s }
+
+    context 'when number is 0' do
+      let(:number) { 0 }
+
+      it { is_expected.to eq '0' }
+    end
+
+    context 'when number is max uint64_t' do
+      let(:number) { 2**64 - 1 }
+
+      it { is_expected.to eq number.to_s }
+    end
+
+    context 'when number is -1' do
+      let(:number) { -1 }
+
+      specify do
+        expect { utoa10 }.to \
+          raise_error RangeError, 'can\'t convert negative number to uint64_t'
+      end
+    end
+
+    context 'when number is greater than max uint64_t' do
+      let(:number) { 2**64 }
+
+      specify do
+        expect { utoa10 }.to raise_error \
+          RangeError, 'bignum too big to convert into `unsigned long long\''
+      end
+    end
+  end
+
+  describe '.itoa10' do
+    subject(:itoa10) { described_class.itoa10 number }
+
+    let(:number) { rand((-2**63)..(2**63 - 1)) }
+
+    it { is_expected.to be_instance_of String }
+    it { is_expected.to be_frozen }
+    it { is_expected.to eq number.to_s }
+
+    context 'when number is 0' do
+      let(:number) { 0 }
+
+      it { is_expected.to eq '0' }
+    end
+
+    context 'when number is 1' do
+      let(:number) { 1 }
+
+      it { is_expected.to eq '1' }
+    end
+
+    context 'when number is -1' do
+      let(:number) { -1 }
+
+      it { is_expected.to eq '-1' }
+    end
+
+    context 'when number is min int64_t' do
+      let(:number) { -2**63 }
+
+      it { is_expected.to eq number.to_s }
+    end
+
+    context 'when number is max int64_t' do
+      let(:number) { 2**63 - 1 }
+
+      it { is_expected.to eq number.to_s }
+    end
+
+    context 'when number is lesser than min uint64_t' do
+      let(:number) { -2**63 - 1 }
+
+      specify do
+        expect { itoa10 }.to \
+          raise_error RangeError, 'bignum too big to convert into `long long\''
+      end
+    end
+
+    context 'when number is greater than max uint64_t' do
+      let(:number) { 2**63 }
+
+      specify do
+        expect { itoa10 }.to \
+          raise_error RangeError, 'bignum too big to convert into `long long\''
+      end
+    end
+  end
 end
