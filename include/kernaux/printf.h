1a0c46b03d954baac40c153d8b4b3385941d4e45
Alex Kotov <kotovalexarian@gmail.com>
2022-01-19 16:58:19 +0500

    Reorder code

--- include/kernaux/printf.h
+++ include/kernaux/printf.h
@@ -51,15 +51,6 @@ int  kernaux_printf(void (*out)(char character, void* arg), void* arg, const cha
 int kernaux_vprintf(void (*out)(char character, void* arg), void* arg, const char* format, va_list va);
 
 /**
- * Tiny sprintf implementation
- * Due to security reasons (buffer overflow) YOU SHOULD CONSIDER USING (V)SNPRINTF INSTEAD!
- * \param buffer A pointer to the buffer where to store the formatted string. MUST be big enough to store the output!
- * \param format A string that specifies the format of the output
- * \return The number of characters that are WRITTEN into the buffer, not counting the terminating null character
- */
-int kernaux_sprintf(char* buffer, const char* format, ...);
-
-/**
  * Tiny snprintf/vsnprintf implementation
  * \param buffer A pointer to the buffer where to store the formatted string
  * \param count The maximum number of characters to store in the buffer, including a terminating null character
@@ -71,6 +62,15 @@ int kernaux_sprintf(char* buffer, const char* format, ...);
  */
 int  kernaux_snprintf(char* buffer, size_t count, const char* format, ...);
 int kernaux_vsnprintf(char* buffer, size_t count, const char* format, va_list va);
+
+/**
+ * Tiny sprintf implementation
+ * Due to security reasons (buffer overflow) YOU SHOULD CONSIDER USING (V)SNPRINTF INSTEAD!
+ * \param buffer A pointer to the buffer where to store the formatted string. MUST be big enough to store the output!
+ * \param format A string that specifies the format of the output
+ * \return The number of characters that are WRITTEN into the buffer, not counting the terminating null character
+ */
+int kernaux_sprintf(char* buffer, const char* format, ...);
 
 #ifdef __cplusplus
 }
