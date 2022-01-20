48622cefcca70ad6eccdda3bfd610889112397a2
Alex Kotov <kotovalexarian@gmail.com>
2022-01-21 00:36:36 +0500

    Add Ruby method KernAux.panic

--- pkgs/ruby/lib/kernaux.rb
+++ pkgs/ruby/lib/kernaux.rb
@@ -6,6 +6,31 @@ require_relative 'kernaux/version'
 # Auxiliary library for kernel development.
 #
 module KernAux
+  DEFAULT_ASSERT_CB = lambda { |file, line, str|
+    raise AssertError, "#{file}:#{line}:#{str}"
+  }
+
+  @assert_cb = DEFAULT_ASSERT_CB
+
+  def self.panic(msg)
+    file, line = caller(1..1).first.split(':')[0..1]
+    assert_do file, Integer(line), msg
+  end
+
+  ##
+  # Our base class for standard errors.
+  #
+  class StandardError < ::StandardError; end
+
+  ##
+  # Out base class for runtime errors.
+  #
+  class RuntimeError < ::RuntimeError; end
+
+  ##
+  # Raised when assertion has failed or panic has been called.
+  #
+  class AssertError < RuntimeError; end
 end
 
 # Native extension
--- pkgs/ruby/spec/lib/kernaux_spec.rb
+++ pkgs/ruby/spec/lib/kernaux_spec.rb
@@ -9,6 +9,35 @@ RSpec.describe KernAux do
     end
   end
 
+  describe '.panic' do
+    subject(:panic) { described_class.panic msg }
+
+    let(:msg) { 'Hello, World!' }
+
+    specify do
+      expect { panic }.to \
+        raise_error described_class::AssertError, /\A#{__FILE__}:\d+:#{msg}\z/
+    end
+
+    context 'when custom assertion callback has beed set' do
+      around do |example|
+        described_class.assert_cb = lambda { |file, line, msg|
+          raise "file: #{file.inspect}, line: #{line}, msg: #{msg.inspect}"
+        }
+        example.run
+      ensure
+        described_class.assert_cb = described_class::DEFAULT_ASSERT_CB
+      end
+
+      specify do
+        expect { panic }.to raise_error(
+          RuntimeError,
+          /\Afile: #{__FILE__.inspect}, line: \d+, msg: #{msg.inspect}\z/,
+        )
+      end
+    end
+  end
+
   if described_class.singleton_class.method_defined? :utoa10
     describe '.utoa10' do
       subject(:utoa10) { described_class.utoa10 number }
