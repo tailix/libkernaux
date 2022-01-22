7ef1988474fb836c9bf1efa9d4a34bc64a98f576
Alex Kotov <kotovalexarian@gmail.com>
2022-01-23 02:10:47 +0500

    Ruby: move assert to separate extension file

--- pkgs/ruby/ext/default/main.c
+++ pkgs/ruby/ext/default/main.c
@@ -11,19 +11,13 @@ static ID rb_intern_LESS, rb_intern_call, rb_intern_freeze;
 static VALUE rb_KernAux = Qnil;
 static VALUE rb_KernAux_Error = Qnil;
 
-static void assert_cb(const char *file, int line, const char *str);
-
+void init_assert();
 #ifdef HAVE_KERNAUX_SNPRINTF
 void init_printf();
 #endif // HAVE_KERNAUX_SNPRINTF
 #ifdef HAVE_KERNAUX_CMDLINE
 void init_cmdline();
 #endif // HAVE_KERNAUX_CMDLINE
-
-static VALUE rb_KernAux_assert_cb(VALUE self);
-static VALUE rb_KernAux_assert_cb_EQ(VALUE self, VALUE assert_cb);
-static
-VALUE rb_KernAux_assert_do(VALUE self, VALUE file, VALUE line, VALUE msg);
 
 #ifdef HAVE_KERNAUX_UTOA10
 static VALUE rb_KernAux_utoa10(VALUE self, VALUE number);
@@ -34,6 +28,7 @@ static VALUE rb_KernAux_itoa10(VALUE self, VALUE number);
 
 void Init_default()
 {
+    init_assert();
 #ifdef HAVE_KERNAUX_SNPRINTF
     init_printf();
 #endif // HAVE_KERNAUX_SNPRINTF
@@ -50,56 +45,12 @@ void Init_default()
     rb_gc_register_mark_object(rb_KernAux_Error =
         rb_define_class_under(rb_KernAux, "Error", rb_eRuntimeError));
 
-    kernaux_assert_cb = assert_cb;
-
-    rb_define_singleton_method(rb_KernAux, "assert_cb",
-                               rb_KernAux_assert_cb, 0);
-    rb_define_singleton_method(rb_KernAux, "assert_cb=",
-                               rb_KernAux_assert_cb_EQ, 1);
-    rb_define_singleton_method(rb_KernAux, "assert_do",
-                               rb_KernAux_assert_do, 3);
-
 #ifdef HAVE_KERNAUX_UTOA10
     rb_define_singleton_method(rb_KernAux, "utoa10", rb_KernAux_utoa10, 1);
 #endif
 #ifdef HAVE_KERNAUX_ITOA10
     rb_define_singleton_method(rb_KernAux, "itoa10", rb_KernAux_itoa10, 1);
 #endif
-}
-
-void assert_cb(const char *const file, const int line, const char *const str)
-{
-    const VALUE assert_cb_rb = rb_iv_get(rb_KernAux, "@assert_cb");
-    if (assert_cb_rb == Qnil) return;
-    const VALUE file_rb = rb_str_new2(file);
-    const VALUE line_rb = INT2FIX(line);
-    const VALUE str_rb = rb_str_new2(str);
-    rb_funcall(assert_cb_rb, rb_intern_call, 3, file_rb, line_rb, str_rb);
-}
-
-VALUE rb_KernAux_assert_cb(const VALUE self)
-{
-    return rb_iv_get(self, "@assert_cb");
-}
-
-VALUE rb_KernAux_assert_cb_EQ(const VALUE self, const VALUE assert_cb_rb)
-{
-    return rb_iv_set(self, "@assert_cb", assert_cb_rb);
-}
-
-VALUE rb_KernAux_assert_do(
-    const VALUE self_rb __attribute__((unused)),
-    VALUE file_rb,
-    const VALUE line_rb,
-    VALUE msg_rb
-) {
-    const char *const file = StringValueCStr(file_rb);
-    const int line = FIX2INT(line_rb);
-    const char *const msg = StringValueCStr(msg_rb);
-
-    kernaux_assert_do(file, line, msg);
-
-    return Qnil;
 }
 
 #ifdef HAVE_KERNAUX_UTOA10
