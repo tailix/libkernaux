31fc0838da618aff881ee6fa9f596db903723410
Alex Kotov <kotovalexarian@gmail.com>
2022-01-23 15:42:06 +0500

    Require semicolon after macros

--- include/kernaux/assert.h
+++ include/kernaux/assert.h
@@ -10,14 +10,14 @@ extern "C" {
 #define KERNAUX_ASSERT(cond) \
     ((cond) ? (void)0 : kernaux_assert_do(__FILE__, __LINE__, #cond))
 
-#define KERNAUX_PANIC_RETURN(msg)         { KERNAUX_PANIC(msg); return;       }
-#define KERNAUX_PANIC_RETVAL(msg, val)    { KERNAUX_PANIC(msg); return (val); }
+#define KERNAUX_PANIC_RETURN(msg)         do { KERNAUX_PANIC(msg);                return;       } while (0)
+#define KERNAUX_PANIC_RETVAL(msg, val)    do { KERNAUX_PANIC(msg);                return (val); } while (0)
 
-#define KERNAUX_ASSERT_RETURN(cond)       { KERNAUX_ASSERT(cond); if (!(cond)) return;       }
-#define KERNAUX_ASSERT_RETVAL(cond, val)  { KERNAUX_ASSERT(cond); if (!(cond)) return (val); }
+#define KERNAUX_ASSERT_RETURN(cond)       do { KERNAUX_ASSERT(cond); if (!(cond)) return;       } while (0)
+#define KERNAUX_ASSERT_RETVAL(cond, val)  do { KERNAUX_ASSERT(cond); if (!(cond)) return (val); } while (0)
 
-#define KERNAUX_NOTNULL_RETURN(cond)      { KERNAUX_ASSERT(cond); if (!(cond)) return;       }
-#define KERNAUX_NOTNULL_RETVAL(cond, val) { KERNAUX_ASSERT(cond); if (!(cond)) return (val); }
+#define KERNAUX_NOTNULL_RETURN(cond)      do { KERNAUX_ASSERT(cond); if (!(cond)) return;       } while (0)
+#define KERNAUX_NOTNULL_RETVAL(cond, val) do { KERNAUX_ASSERT(cond); if (!(cond)) return (val); } while (0)
 
 void kernaux_assert_do(const char *file, int line, const char *str);
 
