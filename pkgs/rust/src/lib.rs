84f8cc9078039b222ab37f12f2004dc0051142c2
Alex Kotov <kotovalexarian@gmail.com>
2022-01-25 03:56:49 +0500

    Rust: add funcs

--- pkgs/rust/src/lib.rs
+++ pkgs/rust/src/lib.rs
@@ -1,8 +1,12 @@
 #[cfg(test)]
-use libc::c_char;
+use libc::{c_char, c_int};
 
 #[link(name = "kernaux")]
 extern "C" {
+    #[cfg(test)]
+    fn kernaux_utoa(value: u64, buffer: *mut c_char, base: c_int) -> *mut c_char;
+    #[cfg(test)]
+    fn kernaux_itoa(value: i64, buffer: *mut c_char, base: c_int) -> *mut c_char;
     #[cfg(test)]
     fn kernaux_utoa10(value: u64, buffer: *mut c_char);
     #[cfg(test)]
@@ -18,6 +22,24 @@ mod tests {
     use super::*;
 
     use std::ffi::CStr;
+
+    #[test]
+    fn utoa() {
+        let mut buffer: [i8; 1000] = [0; 1000];
+        unsafe { kernaux_utoa(0x123, buffer.as_mut_ptr(), 'x' as c_int) };
+        let result =
+            unsafe { CStr::from_ptr(buffer.as_ptr()) }.to_str().unwrap();
+        assert_eq!(result, "123");
+    }
+
+    #[test]
+    fn itoa() {
+        let mut buffer: [i8; 1000] = [0; 1000];
+        unsafe { kernaux_itoa(0x123, buffer.as_mut_ptr(), 'x' as c_int) };
+        let result =
+            unsafe { CStr::from_ptr(buffer.as_ptr()) }.to_str().unwrap();
+        assert_eq!(result, "123");
+    }
 
     #[test]
     fn utoa10() {
