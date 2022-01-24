eff9159a7fa338c30b35c331d130dafdc31d267e
Alex Kotov <kotovalexarian@gmail.com>
2022-01-24 06:59:16 +0500

    Add funcs "kernaux_utoa" and "kernaux_itoa"

--- src/ntoa.c
+++ src/ntoa.c
@@ -4,17 +4,40 @@
 
 #include <kernaux/ntoa.h>
 
-void kernaux_utoa10(uint64_t value, char *buffer)
+#include <stddef.h>
+
+// TODO: example common func, maybe it may become public
+static char *kernaux_utoa(uint64_t value, char *buffer, int base)
 {
+    char alpha = 'a';
+
+    switch (base) {
+    case 'b': base = 2;  break;
+    case 'o': base = 8;  break;
+    case 'd': base = 10; break;
+    case 'H':
+    case 'X':
+        alpha = 'A';
+        // fall through
+    case 'h':
+    case 'x':
+        base = 16;
+        break;
+    }
+
+    if (base < 2 || base > 36) return NULL;
+
     char *pos = buffer;
 
     if (value == 0) *(pos++) = '0';
 
     while (value > 0) {
-        *(pos++) = value % 10 + '0';
-        value = value / 10;
+        const char mod = value % base;
+        *(pos++) = mod < 10 ? mod + '0' : mod - 10 + alpha;
+        value = value / base;
     }
 
+    char *const result = pos;
     *(pos--) = '\0';
 
     while (buffer < pos) {
@@ -22,45 +45,37 @@ void kernaux_utoa10(uint64_t value, char *buffer)
         *(buffer++) = *pos;
         *(pos--)    = tmp;
     }
+
+    return result;
+}
+
+// TODO: example common func, maybe it may become public
+static char *kernaux_itoa(int64_t value, char *buffer, int base)
+{
+    if (value >= 0) {
+        return kernaux_utoa(value, buffer, base);
+    } else {
+        *(buffer++) = '-';
+        return kernaux_utoa(-value, buffer, base);
+    }
+}
+
+void kernaux_utoa10(uint64_t value, char *buffer)
+{
+    kernaux_utoa(value, buffer, 'd');
 }
 
 void kernaux_itoa10(int64_t value, char *buffer)
 {
-    if (value >= 0) {
-        kernaux_utoa10(value, buffer);
-    } else {
-        *(buffer++) = '-';
-        kernaux_utoa10(-value, buffer);
-    }
+    kernaux_itoa(value, buffer, 'd');
 }
 
 void kernaux_utoa16(uint64_t value, char *buffer)
 {
-    char *pos = buffer;
-
-    if (value == 0) *(pos++) = '0';
-
-    while (value > 0) {
-        char mod = value % 16;
-        *(pos++) = mod < 10 ? mod + '0' : mod - 10 + 'a';
-        value = value / 16;
-    }
-
-    *(pos--) = '\0';
-
-    while (buffer < pos) {
-        const char tmp = *buffer;
-        *(buffer++) = *pos;
-        *(pos--)    = tmp;
-    }
+    kernaux_utoa(value, buffer, 'x');
 }
 
 void kernaux_itoa16(int64_t value, char *buffer)
 {
-    if (value >= 0) {
-        kernaux_utoa16(value, buffer);
-    } else {
-        *(buffer++) = '-';
-        kernaux_utoa16(-value, buffer);
-    }
+    kernaux_itoa(value, buffer, 'x');
 }
