b1a295b90ef5d9104ae344e9d12bb5b13a952cb9
Alex Kotov <kotovalexarian@gmail.com>
2022-01-30 16:29:48 +0500

    Ruby: add doc comments

--- pkgs/ruby/lib/kernaux.rb
+++ pkgs/ruby/lib/kernaux.rb
@@ -9,19 +9,56 @@ require_relative 'kernaux/default'
 # Binding to libkernaux - auxiliary library for kernel development.
 #
 module KernAux
-  # Default callback for assertions ({.assert_cb})
+  # Default callback for assertions.
+  # @see .assert_cb
   DEFAULT_ASSERT_CB = @assert_cb = lambda { |file, line, msg|
     raise AssertError, "#{file}:#{line}:#{msg}"
   }
 
-  # Buffer size for {.sprintf1}
-  # @todo make it dynamic
+  # Buffer size for {.sprintf1}.
+  # @todo Make it dynamic.
   SPRINTF1_BUFFER_SIZE = 10_000
 
+  # @!scope class
+
+  ##
+  # @!attribute [rw] assert_cb
+  # Panic callback.
+  #
+  # @see .panic
+  # @see .assert_do
+  ##
+
+  ##
+  # Raise assertion with implicit file and line, retrieved from `caller`, and
+  # explicit message.
+  #
+  # @param msg [String] any message
+  # @return [nil]
+  #
+  # @raise [AssertError] if {.assert_cb} have not been changed.
+  #
+  # @see .assert_do Explicit file and line.
+  #
   def self.panic(msg)
     file, line = caller(1..1).first.split(':')[0..1]
     assert_do file, Integer(line), msg
   end
+
+  ##
+  # @!method assert_do(file, line, msg)
+  # Raise assertion with explicit file, line and message.
+  #
+  # @param file [String] file name, usually from `__FILE__`
+  # @param line [Integer] line number, usually from `__LINE__`
+  # @param msg [String] any message
+  # @return [nil]
+  #
+  # @raise [AssertError] if {.assert_cb} have not been changed.
+  #
+  # @see .panic Implicit file and line.
+
+  # @!parse [ruby] def assert_do(file, line, msg); end
 
   if singleton_class.method_defined? :snprintf1
     def self.sprintf(*args)
@@ -46,6 +83,7 @@ module KernAux
   # Raised when assertion has failed or panic has been called.
   #
   # @see .panic
+  # @see .assert_do
   #
   class AssertError < Error; end
 
