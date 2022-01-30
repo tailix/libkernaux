ad04cf8ba45712301964daf3c896563222086c51
Alex Kotov <kotovalexarian@gmail.com>
2022-01-30 08:18:02 +0500

    Test mruby package with cppcheck

--- pkgs/mruby/src/main.c
+++ pkgs/mruby/src/main.c
@@ -4,5 +4,5 @@ void mrb_mruby_kernaux_gem_final(mrb_state *mrb) {}
 
 void mrb_mruby_kernaux_gem_init(mrb_state *mrb)
 {
-    struct RClass *const rb_KernAux = mrb_define_module(mrb, "KernAux");
+    /* struct RClass *const rb_KernAux = */ mrb_define_module(mrb, "KernAux");
 }
