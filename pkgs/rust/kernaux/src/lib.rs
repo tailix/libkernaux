29e0cd73f9c0b20e60e85ca8ac4e9f8422f87991
Alex Kotov <kotovalexarian@gmail.com>
2022-02-01 08:02:34 +0500

    Rust: implement ntoa::utoa10

--- pkgs/rust/kernaux/src/lib.rs
+++ pkgs/rust/kernaux/src/lib.rs
@@ -1,3 +1,13 @@
 mod ntoa;
 
 pub use ntoa::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ntoa() {
+        assert_eq!(utoa10(123), "123");
+    }
+}
--- pkgs/rust/kernaux/src/ntoa.rs
+++ pkgs/rust/kernaux/src/ntoa.rs
@@ -1,22 +1,20 @@
-pub use kernaux_sys::{
-    ITOA10_BUFFER_SIZE, ITOA16_BUFFER_SIZE, ITOA_BUFFER_SIZE,
-    UTOA10_BUFFER_SIZE, UTOA16_BUFFER_SIZE, UTOA_BUFFER_SIZE,
-};
+use std::ffi::CStr;
+
+use kernaux_sys::{utoa10 as kernaux_utoa10, UTOA10_BUFFER_SIZE};
+
+pub fn utoa10(value: u64) -> String {
+    let mut buffer: [i8; UTOA10_BUFFER_SIZE] = [0; UTOA10_BUFFER_SIZE];
+    unsafe { kernaux_utoa10(value, buffer.as_mut_ptr()) };
+    let result = unsafe { CStr::from_ptr(buffer.as_ptr()) }.to_str().unwrap();
+    String::from(result)
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    use std::ffi::CStr;
-
-    use kernaux_sys::utoa10;
-
     #[test]
     fn test_utoa10() {
-        let mut buffer: [i8; UTOA10_BUFFER_SIZE] = [0; UTOA10_BUFFER_SIZE];
-        unsafe { utoa10(123, buffer.as_mut_ptr()) };
-        let result =
-            unsafe { CStr::from_ptr(buffer.as_ptr()) }.to_str().unwrap();
-        assert_eq!(result, "123");
+        assert_eq!(utoa10(123), "123");
     }
 }
