b2d88a1e19f007cd7c8062f654ef51c7205f5bdb
Alex Kotov <kotovalexarian@gmail.com>
2022-02-03 14:54:25 +0500

    mruby: improve tests

--- pkgs/mruby/test/ntoa.rb
+++ pkgs/mruby/test/ntoa.rb
@@ -14,6 +14,38 @@ def test_itoa(number, base, expected)
   assert_equal expected, result
 end
 
+def test_utoa10(number, expected)
+  result = KernAux.utoa10(number)
+
+  assert_true result.instance_of? String
+  assert_true result.frozen?
+  assert_equal expected, result
+end
+
+def test_itoa10(number, expected)
+  result = KernAux.itoa10(number)
+
+  assert_true result.instance_of? String
+  assert_true result.frozen?
+  assert_equal expected, result
+end
+
+def test_utoa16(number, expected)
+  result = KernAux.utoa16(number)
+
+  assert_true result.instance_of? String
+  assert_true result.frozen?
+  assert_equal expected, result
+end
+
+def test_itoa16(number, expected)
+  result = KernAux.itoa16(number)
+
+  assert_true result.instance_of? String
+  assert_true result.frozen?
+  assert_equal expected, result
+end
+
 assert 'KernAux.utoa' do
   number = Random.rand(2**32 - 1)
   base = 2 + Random.rand(36 - 2)
@@ -128,35 +160,43 @@ assert 'KernAux.itoa' do
 end
 
 assert 'KernAux.utoa10' do
-  assert_equal '0', KernAux.utoa10(0)
-  assert_equal '1', KernAux.utoa10(1)
-  assert_equal '123', KernAux.utoa10(123)
-  assert_equal (2**32 - 1).to_s, KernAux.utoa10(2**32 - 1)
+  test_utoa10 0, '0'
+  test_utoa10 1, '1'
+  test_utoa10 123, '123'
+  test_utoa10 2**32 - 1, (2**32 - 1).to_s
+
+  assert_raise RangeError, 'can\'t convert negative number to uint64_t' do
+    KernAux.utoa10 -1
+  end
 end
 
 assert 'KernAux.itoa10' do
-  assert_equal '0', KernAux.itoa10(0)
-  assert_equal '1', KernAux.itoa10(1)
-  assert_equal '-1', KernAux.itoa10(-1)
-  assert_equal '123', KernAux.itoa10(123)
-  assert_equal '-123', KernAux.itoa10(-123)
-  assert_equal (2**31 - 1).to_s, KernAux.itoa10(2**31 - 1)
-  assert_equal (-2**31).to_s, KernAux.itoa10(-2**31)
+  test_itoa10 0, '0'
+  test_itoa10 1, '1'
+  test_itoa10 -1, '-1'
+  test_itoa10 123, '123'
+  test_itoa10 -123, '-123'
+  test_itoa10 2**31 - 1, (2**31 - 1).to_s
+  test_itoa10 -2**31, (-2**31).to_s
 end
 
 assert 'KernAux.utoa16' do
-  assert_equal '0', KernAux.utoa16(0)
-  assert_equal '1', KernAux.utoa16(1)
-  assert_equal '123', KernAux.utoa16(0x123)
-  assert_equal (2**32 - 1).to_s(16), KernAux.utoa16(2**32 - 1)
+  test_utoa16 0, '0'
+  test_utoa16 1, '1'
+  test_utoa16 0x123, '123'
+  test_utoa16 2**32 - 1, (2**32 - 1).to_s(16)
+
+  assert_raise RangeError, 'can\'t convert negative number to uint64_t' do
+    KernAux.utoa16 -1
+  end
 end
 
 assert 'KernAux.itoa16' do
-  assert_equal '0', KernAux.itoa16(0)
-  assert_equal '1', KernAux.itoa16(1)
-  assert_equal '-1', KernAux.itoa16(-1)
-  assert_equal '123', KernAux.itoa16(0x123)
-  assert_equal '-123', KernAux.itoa16(-0x123)
-  assert_equal (2**31 - 1).to_s(16), KernAux.itoa16(2**31 - 1)
-  assert_equal (-2**31).to_s(16), KernAux.itoa16(-2**31)
+  test_itoa16 0, '0'
+  test_itoa16 1, '1'
+  test_itoa16 -1, '-1'
+  test_itoa16 0x123, '123'
+  test_itoa16 -0x123, '-123'
+  test_itoa16 2**31 - 1, (2**31 - 1).to_s(16)
+  test_itoa16 -2**31, (-2**31).to_s(16)
 end
