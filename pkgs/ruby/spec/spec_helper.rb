a074ac06ad2f6e1b42d70ed19faade631573984c
Alex Kotov <kotovalexarian@gmail.com>
2022-05-23 22:09:56 +0300

    Ruby: move test cases for method KernAux.sprintf to YAML

--- pkgs/ruby/spec/spec_helper.rb
+++ pkgs/ruby/spec/spec_helper.rb
@@ -21,6 +21,8 @@ require 'simplecov'
 
 require 'kernaux'
 
+require 'yaml'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
