f34b66479ac212f48512c58ef2cb2ae6de987136
Alex Kotov <kotovalexarian@gmail.com>
2022-05-27 02:17:29 +0300

    Main: add example "printf_fmt"

--- README.md
+++ README.md
@@ -63,6 +63,7 @@ zero). Work-in-progress APIs can change at any time.
     * [To human](/examples/units_human.c)
   * [printf format parser](/include/kernaux/printf_fmt.h)
     * Code from [https://github.com/mpaland/printf](https://github.com/mpaland/printf). Thank you!
+    * [Example](/examples/printf_fmt.c)
 * Usual functions
   * [libc replacement](/include/kernaux/libc.h) (*stable since* **0.1.0**)
   * [itoa/ftoa replacement](/include/kernaux/ntoa.h) (*stable since* **0.1.0**)
---
+++ examples/printf_fmt.c
@@ -0,0 +1,63 @@
+#include <kernaux/printf_fmt.h>
+
+#include <assert.h>
+
+int main()
+{
+    {
+        const char *format = "012.34f";
+
+        struct KernAux_PrintfFmt_Spec spec = KernAux_PrintfFmt_Spec_create();
+        KernAux_PrintfFmt_Spec_eval_flags(&spec, &format);
+        if (KernAux_PrintfFmt_Spec_eval_width1(&spec, &format)) {
+            KernAux_PrintfFmt_Spec_eval_width2(&spec, &format, 0);
+        }
+        if (KernAux_PrintfFmt_Spec_eval_precision1(&spec, &format)) {
+            KernAux_PrintfFmt_Spec_eval_precision2(&spec, &format, 0);
+        }
+        KernAux_PrintfFmt_Spec_eval_length(&spec, &format);
+        KernAux_PrintfFmt_Spec_eval_type(&spec, &format);
+
+        assert(
+            spec.flags ==
+            (
+                KERNAUX_PRINTF_FMT_FLAGS_ZEROPAD |
+                KERNAUX_PRINTF_FMT_FLAGS_PRECISION
+            )
+        );
+        assert(spec.width == 12);
+        assert(spec.precision == 34);
+        assert(spec.type == KERNAUX_PRINTF_FMT_TYPE_FLOAT);
+        assert(spec.base == 0);
+    }
+
+    {
+        const char *format = " *.*ld";
+
+        struct KernAux_PrintfFmt_Spec spec = KernAux_PrintfFmt_Spec_create();
+        KernAux_PrintfFmt_Spec_eval_flags(&spec, &format);
+        if (KernAux_PrintfFmt_Spec_eval_width1(&spec, &format)) {
+            KernAux_PrintfFmt_Spec_eval_width2(&spec, &format, 12);
+        }
+        if (KernAux_PrintfFmt_Spec_eval_precision1(&spec, &format)) {
+            KernAux_PrintfFmt_Spec_eval_precision2(&spec, &format, 34);
+        }
+        KernAux_PrintfFmt_Spec_eval_length(&spec, &format);
+        KernAux_PrintfFmt_Spec_eval_type(&spec, &format);
+
+        assert(
+            spec.flags ==
+            (
+                KERNAUX_PRINTF_FMT_FLAGS_SPACE |
+                KERNAUX_PRINTF_FMT_FLAGS_LONG |
+                KERNAUX_PRINTF_FMT_FLAGS_PRECISION
+            )
+        );
+        assert(spec.width == 12);
+        assert(spec.precision == 34);
+        assert(spec.type == KERNAUX_PRINTF_FMT_TYPE_INT);
+        assert(spec.base == 10);
+    }
+
+    return 0;
+}
