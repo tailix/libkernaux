3e9c46c15dfbedacfb1103235486458161f3f726
Alex Kotov <kotovalexarian@gmail.com>
2022-06-16 21:34:37 +0300

    Main: examples/io_memstore.c: Add constant

--- examples/io_memstore.c
+++ examples/io_memstore.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #define BUFFER_SIZE 4096
+#define SMALL_BUFFER_SIZE 5
 
 static const char *const hello = "Hello, World!";
 
@@ -23,10 +24,10 @@ void example_main()
 
     {
         struct KernAux_MemStore mem_store;
-        KernAux_MemStore_init(&mem_store, buffer, 5);
+        KernAux_MemStore_init(&mem_store, buffer, SMALL_BUFFER_SIZE);
 
         assert(!KernAux_Store_puts(&mem_store.store, hello));
-        assert(strncmp(buffer, hello, 5) == 0);
+        assert(strncmp(buffer, hello, SMALL_BUFFER_SIZE) == 0);
         assert(strncmp(buffer, hello, strlen(hello)) != 0);
     }
 }
