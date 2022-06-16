b1f4680d831d2fea289b2796dc0799db14af812e
Alex Kotov <kotovalexarian@gmail.com>
2022-06-16 17:35:20 +0300

    mruby: fix common tests for printf (closes #57)

--- bindings/mruby/test/printf.rb
+++ bindings/mruby/test/printf.rb
@@ -1,5 +1,43 @@
 if KernAux::Version.with_printf?
   assert 'KernAux.sprintf' do
+    assert 'integers' do
+      assert_equal 'i: 0',           KernAux.sprintf('i: ',  ['%i', 0])
+      assert_equal 'u: 0',           KernAux.sprintf('u: ',  ['%u', 0])
+      assert_equal 'i: 1',           KernAux.sprintf('i: ',  ['%i', 1])
+      assert_equal 'u: 1',           KernAux.sprintf('u: ',  ['%u', 1])
+      assert_equal 'i: 10',          KernAux.sprintf('i: ',  ['%i', 10])
+      assert_equal 'u: 10',          KernAux.sprintf('u: ',  ['%u', 10])
+      assert_equal 'i: 100',         KernAux.sprintf('i: ',  ['%i', 100])
+      assert_equal 'u: 100',         KernAux.sprintf('u: ',  ['%u', 100])
+      assert_equal 'i: 1000',        KernAux.sprintf('i: ',  ['%i', 1000])
+      assert_equal 'u: 1000',        KernAux.sprintf('u: ',  ['%u', 1000])
+      assert_equal 'i: 10000',       KernAux.sprintf('i: ',  ['%i', 10_000])
+      assert_equal 'u: 10000',       KernAux.sprintf('u: ',  ['%u', 10_000])
+      assert_equal 'i: 100000',      KernAux.sprintf('i: ',  ['%i', 100_000])
+      assert_equal 'u: 100000',      KernAux.sprintf('u: ',  ['%u', 100_000])
+      assert_equal 'i: 1000000',     KernAux.sprintf('i: ',  ['%i', 1_000_000])
+      assert_equal 'u: 1000000',     KernAux.sprintf('u: ',  ['%u', 1_000_000])
+      assert_equal 'i: 10000000',    KernAux.sprintf('i: ',  ['%i', 10_000_000])
+      assert_equal 'u: 10000000',    KernAux.sprintf('u: ',  ['%u', 10_000_000])
+      assert_equal 'i: 100000000',   KernAux.sprintf('i: ',  ['%i', 10**8])
+      assert_equal 'u: 100000000',   KernAux.sprintf('u: ',  ['%u', 10**8])
+      assert_equal 'i: 1000000000',  KernAux.sprintf('i: ',  ['%i', 10**9])
+      assert_equal 'u: 1000000000',  KernAux.sprintf('u: ',  ['%u', 10**9])
+      assert_equal 'i: 2147483647',  KernAux.sprintf('i: ',  ['%i', 2**31 - 1])
+      assert_equal 'u: 2147483647',  KernAux.sprintf('u: ',  ['%u', 2**31 - 1])
+    end
+
+    assert 'integer overflows' do
+      assert_equal 'i: -2147483648', KernAux.sprintf('i: ',  ['%i', 2**31])
+      assert_equal 'u:  2147483648', KernAux.sprintf('u:  ', ['%u', 2**31])
+      assert_equal 'i: -2147483647', KernAux.sprintf('i: ',  ['%i', 2**31 + 1])
+      assert_equal 'u:  2147483649', KernAux.sprintf('u:  ', ['%u', 2**31 + 1])
+      assert_equal 'i: -1',          KernAux.sprintf('i: ',  ['%i', 2**32 - 1])
+      assert_equal 'u:  4294967295', KernAux.sprintf('u:  ', ['%u', 2**32 - 1])
+      assert_equal 'i:  0',          KernAux.sprintf('i:  ', ['%i', 2**32])
+      assert_equal 'u:  0',          KernAux.sprintf('u:  ', ['%u', 2**32])
+    end
+
     [
       ['',      'using regular tests'],
       ['_orig', 'using original tests'],
