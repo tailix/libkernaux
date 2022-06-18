1d838e30db56b6023f4a88c900e23956c6a96eed
Alex Kotov <kotovalexarian@gmail.com>
2022-06-18 13:14:17 +0300

    Add mutex (#69)

---
+++ src/mutex.c
@@ -0,0 +1,22 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <kernaux/assert.h>
+#include <kernaux/mutex.h>
+
+void KernAux_Mutex_lock(const KernAux_Mutex mutex)
+{
+    KERNAUX_ASSERT(mutex);
+    KERNAUX_ASSERT(mutex->lock);
+
+    mutex->lock(mutex->extra_data);
+}
+
+void KernAux_Mutex_unlock(const KernAux_Mutex mutex)
+{
+    KERNAUX_ASSERT(mutex);
+    KERNAUX_ASSERT(mutex->unlock);
+
+    mutex->unlock(mutex->extra_data);
+}
