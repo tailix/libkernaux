2c6187aa4796da936fd3658a70089d235f90dcc1
Alex Kotov <kotovalexarian@gmail.com>
2022-06-20 23:59:56 +0300

    Clean example

--- examples/generic_mutex.c
+++ examples/generic_mutex.c
@@ -1,12 +1,6 @@
-/**
- * This is an implementation of a mutex based on lock file, which is NOT very
- * usable. It however doesn't introduce new dependencies (such as libpthread)
- * while still gives an example of how to inherit the generic type.
- */
-
-//===================
-// lock_file_mutex.c
-//===================
+//============
+// my_mutex.c
+//============
 
 // To not always use macro "KERNAUX_PROTECTED_FIELD" around the names of
 // structure fields you may define "KERNAUX_ACCESS_PROTECTED" before including
@@ -14,66 +8,43 @@
 //
 #define KERNAUX_ACCESS_PROTECTED
 
-//===================
-// lock_file_mutex.h
-//===================
+//============
+// my_mutex.h
+//============
 
 #include <kernaux/generic/mutex.h>
-#include <stdio.h>
 
-#define LOCK_FILE_MUTEX_FILENAME_SLEN 40
-
-typedef struct LockFileMutex {
+typedef struct MyMutex {
     struct KernAux_Mutex mutex;
-    char filename[LOCK_FILE_MUTEX_FILENAME_SLEN + 1];
-    FILE *fd;
-} *LockFileMutex;
+} *MyMutex;
 
-struct LockFileMutex LockFileMutex_create(const char *filename);
+struct MyMutex MyMytex_create();
 
-//===================
-// lock_file_mutex.c
-//===================
+//============
+// my_mutex.c
+//============
 
 #include <kernaux/generic/mutex.h>
-#include <stddef.h>
-#include <string.h>
-#include <unistd.h>
 
-static void LockFileMutex_lock  (void *mutex);
-static void LockFileMutex_unlock(void *mutex);
+static void MyMutex_lock  (void *mutex);
+static void MyMutex_unlock(void *mutex);
 
-struct LockFileMutex LockFileMutex_create(const char *filename)
+struct MyMutex MyMutex_create()
 {
-    struct LockFileMutex lock_file_mutex;
-    lock_file_mutex.mutex.lock = LockFileMutex_lock;
-    lock_file_mutex.mutex.unlock = LockFileMutex_unlock;
-    strncpy(lock_file_mutex.filename, filename, LOCK_FILE_MUTEX_FILENAME_SLEN);
-    lock_file_mutex.filename[LOCK_FILE_MUTEX_FILENAME_SLEN] = '\0';
-    return lock_file_mutex;
+    struct MyMutex my_mutex;
+    my_mutex.mutex.lock = MyMutex_lock;
+    my_mutex.mutex.unlock = MyMutex_unlock;
+    return my_mutex;
 }
 
-void LockFileMutex_lock(void *const mutex)
+void MyMutex_lock(void *const mutex)
 {
-    LockFileMutex lock_file_mutex = mutex;
-    FILE *fd = NULL;
-    while (!fd) {
-        if (!(fd = fopen(lock_file_mutex->filename, "w"))) {
-            sleep(1);
-        }
-    }
-    lock_file_mutex->fd = fd;
+    MyMutex my_mutex = mutex;
 }
 
-void LockFileMutex_unlock(void *const mutex)
+void MyMutex_unlock(void *const mutex)
 {
-    LockFileMutex lock_file_mutex = mutex;
-    FILE *const fd = lock_file_mutex->fd;
-    if (!fd) {
-        return;
-    }
-    lock_file_mutex->fd = NULL;
-    fclose(fd);
+    MyMutex my_mutex = mutex;
 }
 
 //========
@@ -85,15 +56,14 @@ static int shared_counter = 0;
 void example_main()
 {
     // Create mutex
-    struct LockFileMutex lock_file_mutex =
-        LockFileMutex_create("./generic_mutex_file.lock");
+    struct MyMutex my_mutex = MyMutex_create();
 
     // Lock mutex
-    KernAux_Mutex_lock(&lock_file_mutex.mutex);
+    KernAux_Mutex_lock(&my_mutex.mutex);
 
     // Access shared data
     ++shared_counter;
 
     // Unlock mutex
-    KernAux_Mutex_unlock(&lock_file_mutex.mutex);
+    KernAux_Mutex_unlock(&my_mutex.mutex);
 }
