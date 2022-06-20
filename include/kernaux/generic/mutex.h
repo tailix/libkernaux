3e4a1637ab6407581f8d1e60be9297c970ad945c
Alex Kotov <kotovalexarian@gmail.com>
2022-06-20 22:44:26 +0300

    Make mutex attributes protected

--- include/kernaux/generic/mutex.h
+++ include/kernaux/generic/mutex.h
@@ -5,12 +5,14 @@
 extern "C" {
 #endif
 
+#include <kernaux/macro.h>
+
 typedef void (*KernAux_Mutex_Lock  )(struct KernAux_Mutex *mutex);
 typedef void (*KernAux_Mutex_Unlock)(struct KernAux_Mutex *mutex);
 
 typedef struct KernAux_Mutex {
-    KernAux_Mutex_Lock lock;
-    KernAux_Mutex_Unlock unlock;
+    KernAux_Mutex_Lock   KERNAUX_PROTECTED(lock);
+    KernAux_Mutex_Unlock KERNAUX_PROTECTED(unlock);
 } *KernAux_Mutex;
 
 void KernAux_Mutex_lock  (KernAux_Mutex mutex);
