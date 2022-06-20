4a097f313ef58851bc046447feb2504278510217
Alex Kotov <kotovalexarian@gmail.com>
2022-06-20 17:33:34 +0300

    Add <kernaux/macro.h>

---
+++ include/kernaux/macro.h
@@ -0,0 +1,14 @@
+#ifndef KERNAUX_INCLUDED_MACRO
+#define KERNAUX_INCLUDED_MACRO
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define KERNAUX_PRIVATE(id) __PRIVATE_##id
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
