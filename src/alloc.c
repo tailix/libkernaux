c1b7e529dac4dd9c2baf75045ad235d12e16daf5
Alex Kotov <kotovalexarian@gmail.com>
2022-06-20 15:59:28 +0300

    Comment

--- src/alloc.c
+++ src/alloc.c
@@ -20,10 +20,10 @@
 
 #define CONTAINER_OF(ptr, type, member) ((type*)((uintptr_t)(ptr) - offsetof(type, member)))
 
-#define ALIGN_MASK(align) ((align) - 1) // align should be a power of 2
-#define ALIGN_UP(val, align) (((val) + ALIGN_MASK(align)) & ~ALIGN_MASK(align))
+//#define ALIGN_MASK(align) ((align) - 1) // align should be a power of 2
+//#define ALIGN_UP(val, align) (((val) + ALIGN_MASK(align)) & ~ALIGN_MASK(align))
 
-#define PTR_ALIGNMENT (sizeof(void*)) // TODO: align node to this value
+//#define PTR_ALIGNMENT (sizeof(void*)) // TODO: align node to this value
 
 #define LOCK(alloc)                               \
     do {                                          \
