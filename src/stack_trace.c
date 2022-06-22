5e53c46bf1aa8c1bb098f3af6b40de4f112c05d3
Alex Kotov <kotovalexarian@gmail.com>
2022-06-22 16:13:07 +0300

    Get ptr

--- src/stack_trace.c
+++ src/stack_trace.c
@@ -12,8 +12,26 @@
 #include <kernaux/assert.h>
 #include <kernaux/stack_trace.h>
 
+#include <stddef.h>
+
 void kernaux_stack_trace_snprint(char *const buffer, const size_t buffer_size)
 {
     KERNAUX_ASSERT(buffer);
     KERNAUX_ASSERT(buffer_size > 0);
+
+#if !defined(ASM_I386) && !defined(ASM_X86_64)
+    (void)buffer_size; // unused
+    buffer[0] = '\0'; // empty string
+#else
+    size_t *ptr = NULL;
+    __asm__ volatile(
+#if defined(ASM_I386)
+        "movl %%ebp, %0"
+#elif defined(ASM_X86_64)
+        "movq %%rbp, %0"
+#endif
+        : "=g"(ptr)
+        :: "memory"
+    );
+#endif // !defined(ASM_I386) && !defined(ASM_X86_64)
 }
