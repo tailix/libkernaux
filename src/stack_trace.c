39668512207e5f0a95701ae0d0e19f56019d8e4d
Alex Kotov <kotovalexarian@gmail.com>
2022-06-22 17:06:06 +0300

    Remove symbol names

--- src/stack_trace.c
+++ src/stack_trace.c
@@ -15,8 +15,6 @@
 
 #include <stddef.h>
 
-static const char *trace(size_t *offset, size_t ret_addr);
-
 void kernaux_stack_trace_snprint(char *buffer, size_t buffer_size)
 {
     KERNAUX_ASSERT(buffer);
@@ -42,28 +40,14 @@ void kernaux_stack_trace_snprint(char *buffer, size_t buffer_size)
         size_t ret_addr = ptr[1];
         if (!ret_addr) break;
 
-        size_t offset = 0;
-        const char *const name = trace(&offset, ret_addr);
-        int snprintf_result;
-        if (name) {
-            snprintf_result = kernaux_snprintf(
-                buffer,
-                buffer_size,
-                "%lu: 0x%p: %s + 0x%p\n",
-                index,
-                ret_addr,
-                name,
-                offset
-            );
-        } else {
-            snprintf_result = kernaux_snprintf(
-                buffer,
-                buffer_size,
-                "%lu: 0x%p\n",
-                index,
-                ret_addr
-            );
-        }
+        int snprintf_result = kernaux_snprintf(
+            buffer,
+            buffer_size,
+            "%lu: 0x%p\n",
+            index,
+            ret_addr
+        );
+
         if (snprintf_result > 0 && (size_t)snprintf_result < buffer_size) {
             buffer_size -= snprintf_result;
             buffer += snprintf_result;
@@ -73,11 +57,4 @@ void kernaux_stack_trace_snprint(char *buffer, size_t buffer_size)
         ptr = (void*)old_bp;
     }
 #endif // !defined(ASM_I386) && !defined(ASM_X86_64)
-}
-
-static const char *trace(size_t *const offset, const size_t ret_addr)
-{
-    *offset = 0xa8; // example
-    (void)ret_addr;
-    return "foobar"; // example
 }
