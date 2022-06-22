dc8265f89db4cc083ceec100526ab265513ed427
Alex Kotov <kotovalexarian@gmail.com>
2022-06-22 16:24:33 +0300

    Implement stack trace loop

--- src/stack_trace.c
+++ src/stack_trace.c
@@ -14,6 +14,8 @@
 
 #include <stddef.h>
 
+static const char *trace(size_t *offset, size_t ret_addr);
+
 void kernaux_stack_trace_snprint(char *const buffer, const size_t buffer_size)
 {
     KERNAUX_ASSERT(buffer);
@@ -33,5 +35,27 @@ void kernaux_stack_trace_snprint(char *const buffer, const size_t buffer_size)
         : "=g"(ptr)
         :: "memory"
     );
+
+    for (;;) {
+        size_t old_bp   = ptr[0];
+        size_t ret_addr = ptr[1];
+        if (!ret_addr) break;
+
+        size_t offset = 0;
+        const char *const name = trace(&offset, ret_addr);
+        if (name) {
+        } else {
+        }
+
+        if (!old_bp) break;
+        ptr = (void*)old_bp;
+    }
 #endif // !defined(ASM_I386) && !defined(ASM_X86_64)
+}
+
+static const char *trace(size_t *const offset, const size_t ret_addr)
+{
+    *offset = 0;
+    (void)ret_addr;
+    return NULL;
 }
