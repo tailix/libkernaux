2ad235f319db7542e8323e51a1b901166f0c610e
Alex Kotov <kotovalexarian@gmail.com>
2022-06-22 12:39:53 +0300

    Rename memory allocator to free list (#90)

---
+++ include/kernaux/free_list.h
@@ -0,0 +1,37 @@
+#ifndef KERNAUX_INCLUDED_FREE_LIST
+#define KERNAUX_INCLUDED_FREE_LIST
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <kernaux/macro.h>
+#include <kernaux/generic/malloc.h>
+#include <kernaux/generic/mutex.h>
+
+#include <stddef.h>
+
+typedef struct KernAux_FreeList_Node {
+    struct KernAux_FreeList_Node *KERNAUX_PRIVATE_FIELD(next);
+    struct KernAux_FreeList_Node *KERNAUX_PRIVATE_FIELD(prev);
+    size_t KERNAUX_PRIVATE_FIELD(size);
+    char *KERNAUX_PRIVATE_FIELD(block);
+} *KernAux_FreeList_Node;
+
+typedef struct KernAux_FreeList {
+    struct KernAux_Malloc malloc;
+    KernAux_Mutex KERNAUX_PRIVATE_FIELD(mutex);
+    KernAux_FreeList_Node KERNAUX_PRIVATE_FIELD(head);
+} *KernAux_FreeList;
+
+struct KernAux_FreeList KernAux_FreeList_create(KernAux_Mutex mutex);
+void KernAux_FreeList_init(KernAux_FreeList free_list, KernAux_Mutex mutex);
+
+void
+KernAux_FreeList_add_zone(KernAux_FreeList free_list, void *ptr, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ tests/test_free_list.c
@@ -0,0 +1,106 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#define KERNAUX_ACCESS_PRIVATE
+
+#include <kernaux/free_list.h>
+
+#include <assert.h>
+#include <stddef.h>
+#include <string.h>
+
+static void test_default();
+static void test_calloc();
+static void test_calloc_nomem();
+static void test_cross_zone_defrag();
+
+void test_main()
+{
+    test_default();
+    test_calloc();
+    test_calloc_nomem();
+    test_cross_zone_defrag();
+}
+
+void test_default()
+{
+    char memory_block[1000];
+    struct KernAux_FreeList free_list = KernAux_FreeList_create(NULL);
+    KernAux_FreeList_add_zone(&free_list, memory_block, sizeof(memory_block));
+
+    char *const ptr1 = KernAux_Malloc_malloc(&free_list.malloc, 100);
+    assert(ptr1);
+    assert(ptr1 > memory_block);
+    assert(ptr1 < &memory_block[1000]);
+
+    char *const ptr2 = KernAux_Malloc_calloc(&free_list.malloc, 100, 1);
+    assert(ptr2);
+    assert(ptr2 > ptr1);
+    assert(ptr2 < &memory_block[1000]);
+
+    char *const ptr3 = KernAux_Malloc_calloc(&free_list.malloc, 1, 100);
+    assert(ptr3);
+    assert(ptr3 > ptr2);
+    assert(ptr3 < &memory_block[1000]);
+
+    char *const ptr4 = KernAux_Malloc_malloc(&free_list.malloc, 100);
+    assert(ptr4);
+    assert(ptr4 > ptr3);
+    assert(ptr4 < &memory_block[1000]);
+
+    KernAux_Malloc_free(&free_list.malloc, ptr2);
+    KernAux_Malloc_free(&free_list.malloc, ptr3);
+
+    char *const ptr5 = KernAux_Malloc_malloc(&free_list.malloc, 100);
+    assert(ptr5 == ptr2);
+
+    char *const ptr6 = KernAux_Malloc_calloc(&free_list.malloc, 10, 10);
+    assert(ptr6 == ptr3);
+
+    KernAux_Malloc_free(&free_list.malloc, ptr2);
+    KernAux_Malloc_free(&free_list.malloc, ptr3);
+
+    char *const ptr7 = KernAux_Malloc_malloc(&free_list.malloc, 200);
+    assert(ptr7 == ptr2);
+}
+
+void test_calloc()
+{
+    char zone[1000];
+    struct KernAux_FreeList free_list = KernAux_FreeList_create(NULL);
+    KernAux_FreeList_add_zone(&free_list, zone, 1000);
+    char *const ptr = KernAux_Malloc_calloc(&free_list.malloc, 1, 900);
+    for (size_t index = 0; index < 900; ++index) {
+        assert(ptr[index] == 0);
+    }
+    KernAux_Malloc_free(&free_list.malloc, ptr);
+}
+
+void test_calloc_nomem()
+{
+    char zone[1000];
+    struct KernAux_FreeList free_list = KernAux_FreeList_create(NULL);
+    KernAux_FreeList_add_zone(&free_list, zone, sizeof(zone));
+    void *const ptr = KernAux_Malloc_calloc(&free_list.malloc, 1, sizeof(zone));
+    assert(ptr == NULL);
+}
+
+void test_cross_zone_defrag()
+{
+    char zone[1000];
+    struct KernAux_FreeList free_list = KernAux_FreeList_create(NULL);
+    KernAux_FreeList_add_zone(&free_list, &zone[0],   500);
+    KernAux_FreeList_add_zone(&free_list, &zone[500], 500);
+
+    size_t nodes_count = 0;
+    for (
+        KernAux_FreeList_Node item_node = free_list.head;
+        item_node;
+        item_node = item_node->next
+    ) {
+        ++nodes_count;
+    }
+
+    assert(nodes_count == 1);
+}
