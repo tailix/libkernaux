6dfc257f0fe0accaab889bc945f252a8b05fc576
Alex Kotov <kotovalexarian@gmail.com>
2022-06-22 16:36:31 +0300

    Add "tests/test_stack_trace"

---
+++ tests/test_stack_trace.c
@@ -0,0 +1,45 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <kernaux/stack_trace.h>
+
+#include <stdio.h>
+
+static void func1();
+static void func2();
+static void func3();
+static void func4();
+static void func5();
+
+void test_main()
+{
+    func1();
+}
+
+void func1()
+{
+    func2();
+}
+
+void func2()
+{
+    func3();
+}
+
+void func3()
+{
+    func4();
+}
+
+void func4()
+{
+    func5();
+}
+
+void func5()
+{
+    char buffer[1000];
+    kernaux_stack_trace_snprint(buffer, sizeof(buffer));
+    printf("%s", buffer);
+}
