6e991f2f3329a98450f99c153af7ce36478caa97
Alex Kotov <kotovalexarian@gmail.com>
2022-06-23 22:01:00 +0300

    Add docs

--- include/kernaux/drivers/intel_8259_pic.h
+++ include/kernaux/drivers/intel_8259_pic.h
@@ -1,3 +1,15 @@
+/**
+ * @brief A driver for Intel 8259-compatible programmable interrupt controller
+ *
+ * @details
+ * This is not a driver for a random Intel 8259-compatible PIC, but only for a
+ * typical configuration of it that can be found in most PCs.
+ *
+ * @see https://en.wikipedia.org/wiki/Intel_8259
+ * @see https://wiki.osdev.org/8259_PIC
+ * @see https://pdos.csail.mit.edu/6.828/2005/readings/hardware/8259A.pdf
+ */
+
 #ifndef KERNAUX_INCLUDED_DRIVERS_INTEL_8259_PIC
 #define KERNAUX_INCLUDED_DRIVERS_INTEL_8259_PIC
 
