bddf292384767e97be6092330d024fe6532ecbce
Alex Kotov <kotovalexarian@gmail.com>
2022-06-23 13:12:30 +0300

    Return old code

--- include/kernaux/drivers/console.h
+++ include/kernaux/drivers/console.h
@@ -5,6 +5,16 @@
 extern "C" {
 #endif
 
+#include <stddef.h>
+
+void kernaux_drivers_console_putc(char c);
+
+void kernaux_drivers_console_print(const char *s);
+@comment_line_printf@void kernaux_drivers_console_printf(const char *format, ...)
+@comment_line_printf@__attribute__((format(printf, 1, 2)));
+void kernaux_drivers_console_puts(const char *s);
+void kernaux_drivers_console_write(const char *data, size_t size);
+
 #ifdef __cplusplus
 }
 #endif
