aa807ae26775ffe7af3375f46f5f3ab6b910a3c7
Alex Kotov <kotovalexarian@gmail.com>
2022-06-25 15:01:21 +0300

    Add struct KernAux_File

--- include/kernaux/generic/file.h
+++ include/kernaux/generic/file.h
@@ -5,6 +5,16 @@
 extern "C" {
 #endif
 
+#include <kernaux/macro.h>
+
+typedef int (*KernAux_File_Putc)(void *file, int c);
+
+typedef struct KernAux_File {
+    KernAux_File_Putc KERNAUX_PROTECTED_FIELD(putc);
+} *KernAux_File;
+
+int KernAux_File_Putc(KernAux_File file, int c);
+
 #ifdef __cplusplus
 }
 #endif
