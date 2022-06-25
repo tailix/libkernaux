74d14aff8f20d7528eb22e89a00d66240f9dbcf7
Alex Kotov <kotovalexarian@gmail.com>
2022-06-25 18:43:18 +0300

    Use generic file (#97)

--- src/generic/mutex.c
+++ src/generic/mutex.c
@@ -10,7 +10,7 @@ void KernAux_Mutex_lock(const KernAux_Mutex mutex)
     KERNAUX_ASSERT(mutex);
     KERNAUX_ASSERT(mutex->lock);
 
-    mutex->lock(mutex);
+    mutex->lock((void*)mutex);
 }
 
 void KernAux_Mutex_unlock(const KernAux_Mutex mutex)
@@ -18,5 +18,5 @@ void KernAux_Mutex_unlock(const KernAux_Mutex mutex)
     KERNAUX_ASSERT(mutex);
     KERNAUX_ASSERT(mutex->unlock);
 
-    mutex->unlock(mutex);
+    mutex->unlock((void*)mutex);
 }
