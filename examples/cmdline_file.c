dacdfede2bdfc3c46df54ac45ae462a7c23699f4
Alex Kotov <kotovalexarian@gmail.com>
2022-06-27 19:30:47 +0300

    Improve cmdline file API (#103)

--- examples/cmdline_file.c
+++ examples/cmdline_file.c
@@ -5,22 +5,62 @@
 #include <stddef.h>
 #include <string.h>
 
+#define ARG_COUNT_MAX 100
+#define BUFFER_SIZE 4096
+
 static const char *const cmdline = "foo bar\\ baz \"car cdr\"";
 
-static char buffer[4096];
+static void with_args_indices_array();
+static void without_args_indices_array();
 
 void example_main()
 {
+    with_args_indices_array();
+    without_args_indices_array();
+}
+
+void with_args_indices_array()
+{
     char error_msg[KERNAUX_CMDLINE_ERROR_MSG_SIZE_MAX];
     size_t argc;
+    size_t argv[ARG_COUNT_MAX];
+    char buffer[BUFFER_SIZE];
+
     struct KernAux_MemoryFile memory_file =
-        KernAux_MemoryFile_create(buffer, sizeof(buffer), NULL);
+        KernAux_MemoryFile_create(buffer, BUFFER_SIZE, NULL);
 
     assert(kernaux_cmdline_file(
         cmdline,
         error_msg,
         &argc,
-        &memory_file.file
+        &memory_file.file,
+        argv,
+        ARG_COUNT_MAX
+    ));
+
+    assert(strcmp(error_msg, "") == 0);
+    assert(argc == 3);
+    assert(strcmp(&buffer[argv[0]], "foo") == 0);
+    assert(strcmp(&buffer[argv[1]], "bar baz") == 0);
+    assert(strcmp(&buffer[argv[2]], "car cdr") == 0);
+}
+
+void without_args_indices_array()
+{
+    char error_msg[KERNAUX_CMDLINE_ERROR_MSG_SIZE_MAX];
+    size_t argc;
+    char buffer[BUFFER_SIZE];
+
+    struct KernAux_MemoryFile memory_file =
+        KernAux_MemoryFile_create(buffer, BUFFER_SIZE, NULL);
+
+    assert(kernaux_cmdline_file(
+        cmdline,
+        error_msg,
+        &argc,
+        &memory_file.file,
+        NULL,
+        0
     ));
 
     assert(strcmp(error_msg, "") == 0);
--- tests/test_cmdline.c
+++ tests/test_cmdline.c
@@ -2,28 +2,12 @@
 #include "config.h"
 #endif
 
-#include <kernaux/cmdline.h>
-#include <kernaux/memory_file.h>
+#include "cmdline_test.h"
 
-#include <assert.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define ARGV_COUNT_MAX 100
-#define BUFFER_SIZE 4096
-
-static void test(
-    const char *cmdline,
-    size_t argv_count_max,
-    size_t buffer_size,
-
-    bool expected_result,
-    const char *expected_error_msg,
-    size_t expected_argc,
-    const char *const *const expected_argv
-);
 
 static const char *const argv_spaceX3_X3[] = {"   ", "   ", "   "};
 static const char *const argv_backslashX3_X3[] = {"\\\\\\", "\\\\\\", "\\\\\\"};
@@ -132,91 +116,4 @@ void test_main()
         test(buffer, 256, 4096, false, "EOF or buffer overflow", 0, NULL);
         free(buffer);
     }
-}
-
-void test(
-    const char *const cmdline,
-    size_t argv_count_max,
-    size_t buffer_size,
-
-    const bool expected_result,
-    const char *const expected_error_msg,
-    size_t expected_argc,
-    const char *const *const expected_argv
-) {
-    if (argv_count_max == 0) argv_count_max = ARGV_COUNT_MAX;
-    if (buffer_size    == 0) buffer_size    = BUFFER_SIZE;
-
-    char *error_msg = malloc(KERNAUX_CMDLINE_ERROR_MSG_SIZE_MAX);
-    size_t argc = 1234;
-    char **const argv = malloc(sizeof(char*) * argv_count_max);
-    char *const buffer = malloc(buffer_size);
-
-    assert(error_msg);
-    assert(argv);
-    assert(buffer);
-
-    {
-        memset(error_msg, 'x', KERNAUX_CMDLINE_ERROR_MSG_SIZE_MAX);
-        memset(argv,      'x', sizeof(char*) * argv_count_max);
-        memset(buffer,    'x', buffer_size);
-
-        assert(
-            kernaux_cmdline(
-                cmdline,
-                error_msg,
-                &argc,
-                argv,
-                buffer,
-                argv_count_max,
-                buffer_size
-            ) == !!expected_result
-        );
-
-        assert(strcmp(error_msg, expected_error_msg) == 0);
-        assert(argc == expected_argc);
-
-        if (expected_argv) {
-            for (size_t index = 0; index < argc; ++index) {
-                assert(strcmp(argv[index], expected_argv[index]) == 0);
-            }
-        }
-
-        for (size_t index = argc; index < argv_count_max; ++index) {
-            assert(argv[index] == NULL);
-        }
-    }
-
-    if (strcmp(expected_error_msg, "too many args") != 0) {
-        memset(error_msg, 'x', KERNAUX_CMDLINE_ERROR_MSG_SIZE_MAX);
-        memset(argv,      'x', sizeof(char*) * argv_count_max);
-        memset(buffer,    'x', buffer_size);
-
-        struct KernAux_MemoryFile memory_file =
-            KernAux_MemoryFile_create(buffer, buffer_size, NULL);
-
-        assert(
-            kernaux_cmdline_file(
-                cmdline,
-                error_msg,
-                &argc,
-                &memory_file.file
-            ) == !!expected_result
-        );
-
-        assert(strcmp(error_msg, expected_error_msg) == 0);
-        assert(argc == expected_argc);
-
-        if (expected_argv) {
-            const char *arg = buffer;
-            for (size_t index = 0; index < argc; ++index) {
-                assert(strcmp(expected_argv[index], arg) == 0);
-                arg += strlen(arg) + 1;
-            }
-        }
-    }
-
-    free(error_msg);
-    free(argv);
-    free(buffer);
 }
