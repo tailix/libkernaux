58c44b4dc5543014622127cfc3d531ab203084e8
Alex Kotov <kotovalexarian@gmail.com>
2022-11-27 05:10:18 +0400

    Remove common implementations

--- libc/src/stdlib.c
+++ libc/src/stdlib.c
@@ -18,26 +18,21 @@ void exit(const int status)
 void abort()
 {
     // Custom implementation
-    if (kernaux_libc.abort) {
-        kernaux_libc.abort();
-    }
+    if (kernaux_libc.abort) kernaux_libc.abort();
+
     // Default implementation
-    else {
-        exit(EXIT_FAILURE);
-    }
+    exit(EXIT_FAILURE);
 }
 
 void *calloc(const size_t nmemb, const size_t size)
 {
-    // Common implementation
-    const size_t total_size = nmemb * size;
-    if (!total_size) return NULL;
-    if (total_size / nmemb != size) return NULL;
-
     // Custom implementation
     if (kernaux_libc.calloc) return kernaux_libc.calloc(nmemb, size);
 
     // Default implementation
+    const size_t total_size = nmemb * size;
+    if (!total_size) return NULL;
+    if (total_size / nmemb != size) return NULL;
     void *const ptr = malloc(total_size);
     if (ptr) memset(ptr, 0, total_size);
     return ptr;
@@ -45,31 +40,18 @@ void *calloc(const size_t nmemb, const size_t size)
 
 void free(void *const ptr)
 {
-    // Common implementation
-    if (!ptr) return;
-
     // Custom implementation
     kernaux_libc.free(ptr);
 }
 
 void *malloc(const size_t size)
 {
-    // Common implementation
-    if (!size) return NULL;
-
     // Custom implementation
     return kernaux_libc.malloc(size);
 }
 
 void *realloc(void *const ptr, const size_t size)
 {
-    // Common implementation
-    if (!ptr) return malloc(size);
-    if (!size) {
-        free(ptr);
-        return NULL;
-    }
-
     // Custom implementation
     return kernaux_libc.realloc(ptr, size);
 }
