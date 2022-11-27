91bce08aa3161c1708580c69345eee298e9af457
Alex Kotov <kotovalexarian@gmail.com>
2022-11-27 21:03:56 +0400

    `<ctype.h>`: `tolower`, `toupper` (#115)

--- libc/src/ctype.c
+++ libc/src/ctype.c
@@ -9,7 +9,27 @@ int isdigit(const int c)
     return (unsigned)c - '0' < 10;
 }
 
+int islower(const int c)
+{
+    return (unsigned)c - 'a' < 26;
+}
+
 int isspace(const int c)
 {
     return c == ' ' || (unsigned)c - '\t' < 5;
+}
+
+int isupper(const int c)
+{
+    return (unsigned)c - 'A' < 26;
+}
+
+int tolower(const int c)
+{
+    return isupper(c) ? (c + ('a' - 'A')) : c;
+}
+
+int toupper(const int c)
+{
+    return islower(c) ? (c - ('a' - 'A')) : c;
 }
