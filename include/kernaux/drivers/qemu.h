b8991a2d3d46616256f4ac32af85563d49591f18
Alex Kotov <kotovalexarian@gmail.com>
2022-11-28 18:02:14 +0400

    QEMU driver (#118)

---
+++ include/kernaux/drivers/qemu.h
@@ -0,0 +1,14 @@
+#ifndef KERNAUX_INCLUDED_DRIVERS_QEMU
+#define KERNAUX_INCLUDED_DRIVERS_QEMU
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void kernaux_drivers_qemu_poweroff();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
