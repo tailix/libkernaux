060e091be608ad86566eac83e09812a2f9cf1430
Alex Kotov <kotovalexarian@gmail.com>
2022-11-28 15:42:40 +0400

    Rename port I/O funcs

--- include/kernaux/asm/x86.h
+++ include/kernaux/asm/x86.h
@@ -7,6 +7,52 @@ extern "C" {
 
 #include <kernaux/arch/x86.h>
 
+#include <stdint.h>
+
+inline static uint8_t  kernaux_asm_x86_inportb(uint16_t port);
+inline static uint16_t kernaux_asm_x86_inportw(uint16_t port);
+inline static uint32_t kernaux_asm_x86_inportd(uint16_t port);
+
+inline static void kernaux_asm_x86_outportb(uint16_t port, uint8_t  value);
+inline static void kernaux_asm_x86_outportw(uint16_t port, uint16_t value);
+inline static void kernaux_asm_x86_outportd(uint16_t port, uint32_t value);
+
+uint8_t kernaux_asm_x86_inportb(const uint16_t port)
+{
+    register uint8_t result;
+    __asm__ volatile("inb %1, %0" : "=a" (result) : "dN" (port));
+    return result;
+}
+
+uint16_t kernaux_asm_x86_inportw(const uint16_t port)
+{
+    register uint16_t result;
+    __asm__ volatile("inw %1, %0" : "=a" (result) : "dN" (port));
+    return result;
+}
+
+uint32_t kernaux_asm_x86_inportd(const uint16_t port)
+{
+    register uint32_t result;
+    __asm__ volatile("inl %1, %0" : "=a" (result) : "dN" (port));
+    return result;
+}
+
+void kernaux_asm_x86_outportb(const uint16_t port, const uint8_t value)
+{
+    __asm__ volatile("outb %1, %0" : : "dN" (port), "a" (value));
+}
+
+void kernaux_asm_x86_outportw(const uint16_t port, const uint16_t value)
+{
+    __asm__ volatile("outw %1, %0" : : "dN" (port), "a" (value));
+}
+
+void kernaux_asm_x86_outportd(const uint16_t port, const uint32_t value)
+{
+    __asm__ volatile("outl %1, %0" : : "dN" (port), "a" (value));
+}
+
 #ifdef __cplusplus
 }
 #endif
