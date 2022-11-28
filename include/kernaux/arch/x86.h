0c9c8ba3f416274ef1d636358948e830b85091a1
Alex Kotov <kotovalexarian@gmail.com>
2022-11-28 15:33:21 +0400

    Add headers

---
+++ include/kernaux/arch/x86.h
@@ -0,0 +1,12 @@
+#ifndef KERNAUX_INCLUDED_ARCH_X86
+#define KERNAUX_INCLUDED_ARCH_X86
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ include/kernaux/asm/x86.h
@@ -0,0 +1,12 @@
+#ifndef KERNAUX_INCLUDED_ASM_X86
+#define KERNAUX_INCLUDED_ASM_X86
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
