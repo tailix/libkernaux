af5ded9c7b72cad2226f541f08154d51421497df
Alex Kotov <kotovalexarian@gmail.com>
2022-11-28 14:06:24 +0400

    Add func KernAux_Arch_I386_IDTE_set_offset

--- include/kernaux/arch/i386.h
+++ include/kernaux/arch/i386.h
@@ -86,16 +86,22 @@ KERNAUX_PACKING_ATTR;
 
 // Interrupt descriptor table entry
 // TODO: validate this according to spec
-struct KernAux_Arch_I386_IDTE {
+typedef struct KernAux_Arch_I386_IDTE {
     uint16_t offset_low;
     uint16_t selector;
     uint8_t  _zero0;
     uint8_t  flags;
     uint16_t offset_high;
 }
-KERNAUX_PACKING_ATTR;
+KERNAUX_PACKING_ATTR
+*KernAux_Arch_I386_IDTE;
 
 KERNAUX_STATIC_TEST_STRUCT_SIZE(KernAux_Arch_I386_DTE, 8);
+
+void KernAux_Arch_I386_IDTE_set_offset(
+    KernAux_Arch_I386_IDTE idte,
+    uint32_t address
+);
 
 /**
  * @brief Task state segment
--- src/arch/i386.c
+++ src/arch/i386.c
@@ -3,3 +3,12 @@
 #endif
 
 #include <kernaux/arch/i386.h>
+
+void KernAux_Arch_I386_IDTE_set_offset(
+    const KernAux_Arch_I386_IDTE idte,
+    const uint32_t address
+)
+{
+    idte->offset_low  = 0xFFFF & address;
+    idte->offset_high = 0xFFFF & (address >> 16);
+}
