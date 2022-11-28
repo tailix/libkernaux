23693d08c6663ed2475551f31ecf9dffdd7a4ad3
Alex Kotov <kotovalexarian@gmail.com>
2022-11-28 17:15:32 +0400

    Add driver "qemu"

---
+++ include/kernaux/drivers/qemu.h
@@ -0,0 +1,12 @@
+#ifndef KERNAUX_INCLUDED_DRIVERS_QEMU
+#define KERNAUX_INCLUDED_DRIVERS_QEMU
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/drivers/qemu.c
@@ -0,0 +1,8 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <kernaux/drivers/qemu.h>
+
+__attribute__((unused))
+static const int foobar = 0;
