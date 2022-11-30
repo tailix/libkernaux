da1d9fa90ac23c3824356740d4dd05e89caea106
Alex Kotov <kotovalexarian@gmail.com>
2022-12-01 02:42:38 +0400

    Add macro "KERNAUX_MULTIBOOT2_HFIELDS_COMMON"

--- include/kernaux/multiboot2/header_macro.h
+++ include/kernaux/multiboot2/header_macro.h
@@ -5,7 +5,13 @@
 extern "C" {
 #endif
 
+#include <kernaux/macro.h>
 #include <kernaux/multiboot2.h>
+
+#define KERNAUX_MULTIBOOT2_HFIELDS_COMMON(name, type) \
+    struct {                                          \
+        struct KernAux_Multiboot2_HTag_##type tag;    \
+    } KERNAUX_PACKED name;
 
 #ifdef __cplusplus
 }
