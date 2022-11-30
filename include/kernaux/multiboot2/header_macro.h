f48d2a409cc7b5055cb93871ba25b8f5771b680e
Alex Kotov <kotovalexarian@gmail.com>
2022-12-01 02:39:45 +0400

    Add header <kernaux/multiboot2/header_macro.h>

---
+++ include/kernaux/multiboot2/header_macro.h
@@ -0,0 +1,14 @@
+#ifndef KERNAUX_INCLUDED_MULTIBOOT2_HEADER_MACRO
+#define KERNAUX_INCLUDED_MULTIBOOT2_HEADER_MACRO
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <kernaux/multiboot2.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
