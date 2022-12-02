7ac7ab3e2578157db69eebcf077aef595abd4b4e
Alex Kotov <kotovalexarian@gmail.com>
2022-12-02 18:54:12 +0400

    Fix examples

--- examples/bootloader-multiboot2-grub/main.c
+++ examples/bootloader-multiboot2-grub/main.c
@@ -1,7 +1,7 @@
 #include <stdarg.h>
 #include <stdint.h>
 
-#include <kernaux/asm/i386.h>
+#include <kernaux/asm/x86.h>
 #include <kernaux/drivers/console.h>
 #include <kernaux/multiboot2.h>
 
@@ -48,7 +48,7 @@ void main(
 
 void poweroff()
 {
-    kernaux_asm_i386_outportw(0x604, 0x2000);
+    kernaux_asm_x86_outportw(0x604, 0x2000);
 }
 
 void panic(const char *const str)
