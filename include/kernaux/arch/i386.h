a2a28a6dde6cf08783dbd33225a6391193c6f9d4
Alex Kotov <kotovalexarian@gmail.com>
2022-12-08 01:46:30 +0400

    Define CR4 for both i386 and x86_64

--- include/kernaux/arch/i386.h
+++ include/kernaux/arch/i386.h
@@ -55,61 +55,8 @@ KERNAUX_STATIC_TEST_UNION_SIZE(KernAux_Arch_I386_CR0, 4);
  * @see https://en.wikipedia.org/wiki/Control_register#CR4
  * @see https://wiki.osdev.org/CPU_Registers_x86#CR4
  */
-union KernAux_Arch_I386_CR4 {
-    uint32_t number;
-#ifdef KERNAUX_BITFIELDS
-    struct {
-        bool     vme        : 1; // 0:  Virtual-8086 Mode Extensions
-        bool     pvi        : 1; // 1:  Protected-Mode Virtual Interrupts
-        bool     tsd        : 1; // 2:  Time Stamp Disable
-        bool     de         : 1; // 3:  Debugging Extensions
-        bool     pse        : 1; // 4:  Page Size Extension
-        bool     pae        : 1; // 5:  Physical Address Extension
-        bool     mce        : 1; // 6:  Machine-Check Exception
-        bool     pge        : 1; // 7:  Page Global Enable
-        bool     pce        : 1; // 8:  Performance-Monitoring Counter Enabled
-        bool     osfxsr     : 1; // 9:  Operating System Support for FXSAVE and FXRSTOR instructions
-        bool     osxmmexcpt : 1; // 10: Operating System Support for Unmasked SIMD Floating-Point Exceptions
-        bool     umip       : 1; // 11: User-Mode Instruction Prevention
-        unsigned _0         : 1;
-        bool     vmxe       : 1; // 13: VME (Virtual Machine Extensions) Enable
-        bool     smxe       : 1; // 14: SME (Safer Mode Extensions) Enable
-        unsigned _1         : 1;
-        bool     fsgsbase   : 1; // 16: FSGSBASE Enable (enable the instructions RDFSBASE, RDGSBASE, WRFSBASE, and WRGSBASE)
-        bool     pcide      : 1; // 17: PCID Enable
-        bool     osxsave    : 1; // 18: XSAVE and Processor Extended States Enable
-        unsigned _2         : 1;
-        bool     smep       : 1; // 20: SMEP (Supervisor Mode Execution Protection) Enable
-        bool     smap       : 1; // 21: SMAP (Supervisor Mode Access Prevention) Enable
-        bool     pke        : 1; // 22: Protection Key Enable
-        unsigned _3         : 10;
-    } KERNAUX_PACKED bitfields;
-#endif
-}
-KERNAUX_PACKED;
-
+KERNAUX_ARCH_X86_DEFINE_CR4(I386, uint32_t);
 KERNAUX_STATIC_TEST_UNION_SIZE(KernAux_Arch_I386_CR4, 4);
-
-#define KERNAUX_ARCH_I386_CR4_VME        KERNAUX_BITS32(0)  // Virtual-8086 Mode Extensions
-#define KERNAUX_ARCH_I386_CR4_PVI        KERNAUX_BITS32(1)  // Protected-Mode Virtual Interrupts
-#define KERNAUX_ARCH_I386_CR4_TSD        KERNAUX_BITS32(2)  // Time Stamp Disable
-#define KERNAUX_ARCH_I386_CR4_DE         KERNAUX_BITS32(3)  // Debugging Extensions
-#define KERNAUX_ARCH_I386_CR4_PSE        KERNAUX_BITS32(4)  // Page Size Extension
-#define KERNAUX_ARCH_I386_CR4_PAE        KERNAUX_BITS32(5)  // Physical Address Extension
-#define KERNAUX_ARCH_I386_CR4_MCE        KERNAUX_BITS32(6)  // Machine-Check Exception
-#define KERNAUX_ARCH_I386_CR4_PGE        KERNAUX_BITS32(7)  // Page Global Enable
-#define KERNAUX_ARCH_I386_CR4_PCE        KERNAUX_BITS32(8)  // Performance-Monitoring Counter Enabled
-#define KERNAUX_ARCH_I386_CR4_OSFXSR     KERNAUX_BITS32(9)  // Operating System Support for FXSAVE and FXRSTOR instructions
-#define KERNAUX_ARCH_I386_CR4_OSXMMEXCPT KERNAUX_BITS32(10) // Operating System Support for Unmasked SIMD Floating-Point Exceptions
-#define KERNAUX_ARCH_I386_CR4_UMIP       KERNAUX_BITS32(11) // User-Mode Instruction Prevention
-#define KERNAUX_ARCH_I386_CR4_VMXE       KERNAUX_BITS32(13) // VME (Virtual Machine Extensions) Enable
-#define KERNAUX_ARCH_I386_CR4_SMXE       KERNAUX_BITS32(14) // SME (Safer Mode Extensions) Enable
-#define KERNAUX_ARCH_I386_CR4_FSGSBASE   KERNAUX_BITS32(16) // FSGSBASE Enable (enable the instructions RDFSBASE, RDGSBASE, WRFSBASE, and WRGSBASE)
-#define KERNAUX_ARCH_I386_CR4_PCIDE      KERNAUX_BITS32(17) // PCID Enable
-#define KERNAUX_ARCH_I386_CR4_OSXSAVE    KERNAUX_BITS32(18) // XSAVE and Processor Extended States Enable
-#define KERNAUX_ARCH_I386_CR4_SMEP       KERNAUX_BITS32(20) // SMEP (Supervisor Mode Execution Protection) Enable
-#define KERNAUX_ARCH_I386_CR4_SMAP       KERNAUX_BITS32(21) // SMAP (Supervisor Mode Access Prevention) Enable
-#define KERNAUX_ARCH_I386_CR4_PKE        KERNAUX_BITS32(22) // Protection Key Enable
 
 // Global, local or interrupt descriptor table register
 // TODO: validate this according to spec
--- include/kernaux/arch/x86_64.h
+++ include/kernaux/arch/x86_64.h
@@ -20,6 +20,20 @@ extern "C" {
 KERNAUX_ARCH_X86_DEFINE_CR0(X86_64, uint64_t);
 KERNAUX_STATIC_TEST_UNION_SIZE(KernAux_Arch_X86_64_CR0, 8);
 
+/**
+ * @brief CR4 bits
+ *
+ * @details
+ * Contains a group of flags that enable several architectural extensions,
+ * and indicate operating system or executive support for specific processor
+ * capabilities.
+ *
+ * @see https://en.wikipedia.org/wiki/Control_register#CR4
+ * @see https://wiki.osdev.org/CPU_Registers_x86#CR4
+ */
+KERNAUX_ARCH_X86_DEFINE_CR4(X86, uint64_t);
+KERNAUX_STATIC_TEST_UNION_SIZE(KernAux_Arch_X86_CR4, 8);
+
 #ifdef __cplusplus
 }
 #endif
