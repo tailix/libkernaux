842402e3fec63dba4572718bbaf03dc31770ee78
Alex Kotov <kotovalexarian@gmail.com>
2022-12-08 00:59:56 +0400

    Finish CR0 and CR4

--- include/kernaux/arch/i386.h
+++ include/kernaux/arch/i386.h
@@ -31,19 +31,24 @@ extern "C" {
 
 #include <kernaux/macro/packing_start.run>
 
-// CR0 bits
-#define KERNAUX_ARCH_I386_CR0_PE KERNAUX_BITS32(0)  // 0:  Protected Mode Enable
-#define KERNAUX_ARCH_I386_CR0_MP KERNAUX_BITS32(1)  // 1:  Monitor co-processor
-#define KERNAUX_ARCH_I386_CR0_EM KERNAUX_BITS32(2)  // 2:  x87 FPU Emulation
-#define KERNAUX_ARCH_I386_CR0_TS KERNAUX_BITS32(3)  // 3:  Task switched
-#define KERNAUX_ARCH_I386_CR0_ET KERNAUX_BITS32(4)  // 4:  Extension type
-#define KERNAUX_ARCH_I386_CR0_NE KERNAUX_BITS32(5)  // 5:  Numeric error
-#define KERNAUX_ARCH_I386_CR0_WP KERNAUX_BITS32(16) // 16: Write protect
-#define KERNAUX_ARCH_I386_CR0_AM KERNAUX_BITS32(18) // 18: Alignment mask
-#define KERNAUX_ARCH_I386_CR0_NW KERNAUX_BITS32(29) // 29: Not-write trough
-#define KERNAUX_ARCH_I386_CR0_CD KERNAUX_BITS32(30) // 30: Cache disable
-#define KERNAUX_ARCH_I386_CR0_PG KERNAUX_BITS32(31) // 31: Paging
+#define KERNAUX_ARCH_I386_CR0_PE KERNAUX_BITS32(0)  // Protected Mode Enable
+#define KERNAUX_ARCH_I386_CR0_MP KERNAUX_BITS32(1)  // Monitor co-processor
+#define KERNAUX_ARCH_I386_CR0_EM KERNAUX_BITS32(2)  // x87 FPU Emulation
+#define KERNAUX_ARCH_I386_CR0_TS KERNAUX_BITS32(3)  // Task switched
+#define KERNAUX_ARCH_I386_CR0_ET KERNAUX_BITS32(4)  // Extension type
+#define KERNAUX_ARCH_I386_CR0_NE KERNAUX_BITS32(5)  // Numeric error
+#define KERNAUX_ARCH_I386_CR0_WP KERNAUX_BITS32(16) // Write protect
+#define KERNAUX_ARCH_I386_CR0_AM KERNAUX_BITS32(18) // Alignment mask
+#define KERNAUX_ARCH_I386_CR0_NW KERNAUX_BITS32(29) // Not-write trough
+#define KERNAUX_ARCH_I386_CR0_CD KERNAUX_BITS32(30) // Cache disable
+#define KERNAUX_ARCH_I386_CR0_PG KERNAUX_BITS32(31) // Paging
 
+/**
+ * @brief CR0 bits
+ *
+ * @see https://en.wikipedia.org/wiki/Control_register#CR0
+ * @see https://wiki.osdev.org/CPU_Registers_x86#CR0
+ */
 union KernAux_Arch_I386_CR0 {
     uint32_t number;
 #ifdef KERNAUX_BITFIELDS
@@ -54,9 +59,11 @@ union KernAux_Arch_I386_CR0 {
         bool     ts : 1; // 3:  Task switched
         bool     et : 1; // 4:  Extension type
         bool     ne : 1; // 5:  Numeric error
-        unsigned __ : 21;
+        unsigned _0 : 10;
         bool     wp : 1; // 16: Write protect
+        unsigned _1 : 1;
         bool     am : 1; // 18: Alignment mask
+        unsigned _2 : 10;
         bool     nw : 1; // 29: Not-write trough
         bool     cd : 1; // 30: Cache disable
         bool     pg : 1; // 31: Paging
@@ -67,30 +74,65 @@ KERNAUX_PACKED;
 
 KERNAUX_STATIC_TEST_UNION_SIZE(KernAux_Arch_I386_CR0, 4);
 
-// Some CR4 bits
-#define KERNAUX_ARCH_I386_CR4_VME KERNAUX_BITS32(0) // 0: Virtual 8086 Mode Extensions
-#define KERNAUX_ARCH_I386_CR4_PVI KERNAUX_BITS32(1) // 1: Protected-mode Virtual Interrupts
-#define KERNAUX_ARCH_I386_CR4_TSD KERNAUX_BITS32(2) // 2: Time Stamp Disable
-#define KERNAUX_ARCH_I386_CR4_DE  KERNAUX_BITS32(3) // 3: Debugging Extensions
-#define KERNAUX_ARCH_I386_CR4_PSE KERNAUX_BITS32(4) // 4: Page Size Extension
-#define KERNAUX_ARCH_I386_CR4_PAE KERNAUX_BITS32(5) // 5: Physical Address Extension
-#define KERNAUX_ARCH_I386_CR4_MCE KERNAUX_BITS32(6) // 6: Machine Check Exception
-#define KERNAUX_ARCH_I386_CR4_PGE KERNAUX_BITS32(7) // 7: Page Global Enabled
-// TODO: bits 8-31
+#define KERNAUX_ARCH_I386_CR4_VME        KERNAUX_BITS32(0)  // Virtual 8086 Mode Extensions
+#define KERNAUX_ARCH_I386_CR4_PVI        KERNAUX_BITS32(1)  // Protected-mode Virtual Interrupts
+#define KERNAUX_ARCH_I386_CR4_TSD        KERNAUX_BITS32(2)  // Time Stamp Disable
+#define KERNAUX_ARCH_I386_CR4_DE         KERNAUX_BITS32(3)  // Debugging Extensions
+#define KERNAUX_ARCH_I386_CR4_PSE        KERNAUX_BITS32(4)  // Page Size Extension
+#define KERNAUX_ARCH_I386_CR4_PAE        KERNAUX_BITS32(5)  // Physical Address Extension
+#define KERNAUX_ARCH_I386_CR4_MCE        KERNAUX_BITS32(6)  // Machine Check Exception
+#define KERNAUX_ARCH_I386_CR4_PGE        KERNAUX_BITS32(7)  // Page Global Enabled
+#define KERNAUX_ARCH_I386_CR4_PCE        KERNAUX_BITS32(8)  // Performance-Monitoring Counter Enabled
+#define KERNAUX_ARCH_I386_CR4_OSFXSR     KERNAUX_BITS32(9)  // Operating system support for FXSAVE and FXRSTOR instructions
+#define KERNAUX_ARCH_I386_CR4_OSXMMEXCPT KERNAUX_BITS32(10) // Operating system support for Unmasked SIMD Floating-Point Exceptions
+#define KERNAUX_ARCH_I386_CR4_UMIP       KERNAUX_BITS32(11) // User-Mode Instruction Prevention
+#define KERNAUX_ARCH_I386_CR4_VMXE       KERNAUX_BITS32(13) // Virtual Machine Extensions Enabled
+#define KERNAUX_ARCH_I386_CR4_SMXE       KERNAUX_BITS32(14) // Safer Mode Extensions Enabled
+#define KERNAUX_ARCH_I386_CR4_FSGSBASE   KERNAUX_BITS32(16) // Enable the instructions RDFSBASE, RDGSBASE, WRFSBASE, and WRGSBASE
+#define KERNAUX_ARCH_I386_CR4_PCIDE      KERNAUX_BITS32(17) // PCID Enabled
+#define KERNAUX_ARCH_I386_CR4_OSXSAVE    KERNAUX_BITS32(18) // XSAVE and Processor Extended States Enabled
+#define KERNAUX_ARCH_I386_CR4_SMEP       KERNAUX_BITS32(20) // Supervisor Mode Execution Protection Enabled
+#define KERNAUX_ARCH_I386_CR4_SMAP       KERNAUX_BITS32(21) // Supervisor Mode Access Prevention Enable
+#define KERNAUX_ARCH_I386_CR4_PKE        KERNAUX_BITS32(22) // Protection Key Enabled
+#define KERNAUX_ARCH_I386_CR4_CET        KERNAUX_BITS32(23) // Control-flow Enforcement Technology
+#define KERNAUX_ARCH_I386_CR4_PKS        KERNAUX_BITS32(24) // Enable Protection Keys for Supervisor-Mode Pages
 
+/**
+ * @brief CR4 bits
+ *
+ * @see https://en.wikipedia.org/wiki/Control_register#CR4
+ * @see https://wiki.osdev.org/CPU_Registers_x86#CR4
+ */
 union KernAux_Arch_I386_CR4 {
     uint32_t number;
 #ifdef KERNAUX_BITFIELDS
     struct {
-        bool     vme : 1; // 0: Virtual 8086 Mode Extensions
-        bool     pvi : 1; // 1: Protected-mode Virtual Interrupts
-        bool     tsd : 1; // 2: Time Stamp Disable
-        bool     de  : 1; // 3: Debugging Extensions
-        bool     pse : 1; // 4: Page Size Extension
-        bool     pae : 1; // 5: Physical Address Extension
-        bool     mce : 1; // 6: Machine Check Exception
-        bool     pge : 1; // 7: Page Global Enabled
-        unsigned ___ : 24;
+        bool     vme        : 1; // 0:  Virtual 8086 Mode Extensions
+        bool     pvi        : 1; // 1:  Protected-mode Virtual Interrupts
+        bool     tsd        : 1; // 2:  Time Stamp Disable
+        bool     de         : 1; // 3:  Debugging Extensions
+        bool     pse        : 1; // 4:  Page Size Extension
+        bool     pae        : 1; // 5:  Physical Address Extension
+        bool     mce        : 1; // 6:  Machine Check Exception
+        bool     pge        : 1; // 7:  Page Global Enabled
+        bool     pce        : 1; // 8:  Performance-Monitoring Counter Enabled
+        bool     osfxsr     : 1; // 9:  Operating system support for FXSAVE and FXRSTOR instructions
+        bool     osxmmexcpt : 1; // 10: Operating system support for Unmasked SIMD Floating-Point Exceptions
+        bool     umip       : 1; // 11: User-Mode Instruction Prevention
+        unsigned _0         : 1;
+        bool     vmxe       : 1; // 13: Virtual Machine Extensions Enabled
+        bool     smxe       : 1; // 14: Safer Mode Extensions Enabled
+        unsigned _1         : 1;
+        bool     fsgsbase   : 1; // 16: Enable the instructions RDFSBASE, RDGSBASE, WRFSBASE, and WRGSBASE
+        bool     pcide      : 1; // 17: PCID Enabled
+        bool     osxsave    : 1; // 18: XSAVE and Processor Extended States Enabled
+        unsigned _2         : 1;
+        bool     smep       : 1; // 20: Supervisor Mode Execution Protection Enabled
+        bool     smap       : 1; // 21: Supervisor Mode Access Prevention Enable
+        bool     pke        : 1; // 22: Protection Key Enabled
+        bool     cet        : 1; // 23: Control-flow Enforcement Technology
+        bool     pks        : 1; // 24: Enable Protection Keys for Supervisor-Mode Pages
+        unsigned _3         : 8;
     } KERNAUX_PACKED bitfields;
 #endif
 }
