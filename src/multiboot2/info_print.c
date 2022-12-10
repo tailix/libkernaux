eea1117af6edafb059b130043a01b26755264309
Alex Kotov <kotovalexarian@gmail.com>
2022-12-10 16:51:54 +0400

    Fix warnings

--- src/multiboot2/info_print.c
+++ src/multiboot2/info_print.c
@@ -3,6 +3,7 @@
 #endif
 
 #include <kernaux/assert.h>
+#include <kernaux/macro.h>
 #include <kernaux/multiboot2.h>
 
 #include <stddef.h>
@@ -23,8 +24,8 @@ void KernAux_Multiboot2_Info_print(
     KERNAUX_ASSERT(display);
 
     PRINTLN("Multiboot 2 info");
-    PRINTLNF("  size: %u", multiboot2_info->total_size);
-    PRINTLNF("  reserved1: %u", multiboot2_info->reserved1);
+    PRINTLNF("  size: %"KERNAUX_PRIu32, multiboot2_info->total_size);
+    PRINTLNF("  reserved1: %"KERNAUX_PRIu32, multiboot2_info->reserved1);
 
     const struct KernAux_Multiboot2_ITagBase *tag_base =
         (struct KernAux_Multiboot2_ITagBase*)
@@ -58,7 +59,7 @@ void KernAux_Multiboot2_ITagBase_print(
         KernAux_Multiboot2_ITag_to_str(tag_base->type)
     );
 
-    PRINTLNF("  size: %u", tag_base->size);
+    PRINTLNF("  size: %"KERNAUX_PRIu32, tag_base->size);
 
     switch (tag_base->type) {
     case KERNAUX_MULTIBOOT2_ITAG_NONE:
@@ -80,8 +81,8 @@ void KernAux_Multiboot2_ITagBase_print(
             const struct KernAux_Multiboot2_ITag_Module *const tag_module =
                 (struct KernAux_Multiboot2_ITag_Module*)tag_base;
 
-            PRINTLNF("  start: %u",   tag_module->mod_start);
-            PRINTLNF("  end: %u",     tag_module->mod_end);
+            PRINTLNF("  start: %"KERNAUX_PRIu32,   tag_module->mod_start);
+            PRINTLNF("  end: %"KERNAUX_PRIu32,     tag_module->mod_end);
             PRINTLNF("  cmdline: %s", KERNAUX_MULTIBOOT2_DATA(tag_module));
         }
         break;
@@ -90,8 +91,8 @@ void KernAux_Multiboot2_ITagBase_print(
             const struct KernAux_Multiboot2_ITag_BasicMemoryInfo *const tag_bmi =
                 (struct KernAux_Multiboot2_ITag_BasicMemoryInfo*)tag_base;
 
-            PRINTLNF("  mem lower: %u", tag_bmi->mem_lower);
-            PRINTLNF("  mem upper: %u", tag_bmi->mem_upper);
+            PRINTLNF("  mem lower: %"KERNAUX_PRIu32, tag_bmi->mem_lower);
+            PRINTLNF("  mem upper: %"KERNAUX_PRIu32, tag_bmi->mem_upper);
         }
         break;
     case KERNAUX_MULTIBOOT2_ITAG_BIOS_BOOT_DEVICE:
@@ -99,9 +100,9 @@ void KernAux_Multiboot2_ITagBase_print(
             const struct KernAux_Multiboot2_ITag_BIOSBootDevice *const tag_bbd =
                 (struct KernAux_Multiboot2_ITag_BIOSBootDevice*)tag_base;
 
-            PRINTLNF("  bios dev: %u", tag_bbd->bios_dev);
-            PRINTLNF("  partition: %u", tag_bbd->partition);
-            PRINTLNF("  sub_partition: %u", tag_bbd->sub_partition);
+            PRINTLNF("  bios dev: %"KERNAUX_PRIu32, tag_bbd->bios_dev);
+            PRINTLNF("  partition: %"KERNAUX_PRIu32, tag_bbd->partition);
+            PRINTLNF("  sub_partition: %"KERNAUX_PRIu32, tag_bbd->sub_partition);
         }
         break;
     case KERNAUX_MULTIBOOT2_ITAG_MEMORY_MAP:
@@ -115,10 +116,10 @@ void KernAux_Multiboot2_ITagBase_print(
             const struct KernAux_Multiboot2_ITag_VBEInfo *const tag_vbe =
                 (struct KernAux_Multiboot2_ITag_VBEInfo*)tag_base;
 
-            PRINTLNF("  VBE mode: %hu",          tag_vbe->vbe_mode);
-            PRINTLNF("  VBE interface seg: %hu", tag_vbe->vbe_interface_seg);
-            PRINTLNF("  VBE interface off: %hu", tag_vbe->vbe_interface_off);
-            PRINTLNF("  VBE interface len: %hu", tag_vbe->vbe_interface_len);
+            PRINTLNF("  VBE mode: %"KERNAUX_PRIu16,          tag_vbe->vbe_mode);
+            PRINTLNF("  VBE interface seg: %"KERNAUX_PRIu16, tag_vbe->vbe_interface_seg);
+            PRINTLNF("  VBE interface off: %"KERNAUX_PRIu16, tag_vbe->vbe_interface_off);
+            PRINTLNF("  VBE interface len: %"KERNAUX_PRIu16, tag_vbe->vbe_interface_len);
         }
         break;
     case KERNAUX_MULTIBOOT2_ITAG_FRAMEBUFFER_INFO:
@@ -126,16 +127,13 @@ void KernAux_Multiboot2_ITagBase_print(
             const struct KernAux_Multiboot2_ITag_FramebufferInfo *const tag_fb =
                 (struct KernAux_Multiboot2_ITag_FramebufferInfo*)tag_base;
 
-            PRINTLNF(
-                INT_IF("  framebuffer addr: %llu", "  framebuffer addr: %lu"),
-                tag_fb->framebuffer_addr
-            );
-            PRINTLNF("  framebuffer pitch: %u",  tag_fb->framebuffer_pitch);
-            PRINTLNF("  framebuffer width: %u",  tag_fb->framebuffer_width);
-            PRINTLNF("  framebuffer height: %u", tag_fb->framebuffer_height);
-            PRINTLNF("  framebuffer bpp: %u",    tag_fb->framebuffer_bpp);
-            PRINTLNF("  framebuffer type: %u",   tag_fb->framebuffer_type);
-            PRINTLNF("  reserved1: %u",          tag_fb->reserved1);
+            PRINTLNF("  framebuffer addr: %"KERNAUX_PRIu64,   tag_fb->framebuffer_addr);
+            PRINTLNF("  framebuffer pitch: %"KERNAUX_PRIu32,  tag_fb->framebuffer_pitch);
+            PRINTLNF("  framebuffer width: %"KERNAUX_PRIu32,  tag_fb->framebuffer_width);
+            PRINTLNF("  framebuffer height: %"KERNAUX_PRIu32, tag_fb->framebuffer_height);
+            PRINTLNF("  framebuffer bpp: %"KERNAUX_PRIu8,     tag_fb->framebuffer_bpp);
+            PRINTLNF("  framebuffer type: %"KERNAUX_PRIu8,    tag_fb->framebuffer_type);
+            PRINTLNF("  reserved1: %"KERNAUX_PRIu8,           tag_fb->reserved1);
         }
         break;
     case KERNAUX_MULTIBOOT2_ITAG_ELF_SYMBOLS:
@@ -149,15 +147,15 @@ void KernAux_Multiboot2_ITagBase_print(
             const struct KernAux_Multiboot2_ITag_APMTable *const tag_apm =
                 (struct KernAux_Multiboot2_ITag_APMTable*)tag_base;
 
-            PRINTLNF("  version: %hu",     tag_apm->version);
-            PRINTLNF("  cseg: %hu",        tag_apm->cseg);
-            PRINTLNF("  offset: %u",       tag_apm->offset);
-            PRINTLNF("  cseg 16: %hu",     tag_apm->cseg_16);
-            PRINTLNF("  dseg: %hu",        tag_apm->dseg);
-            PRINTLNF("  flags: %hu",       tag_apm->flags);
-            PRINTLNF("  cseg len: %hu",    tag_apm->cseg_len);
-            PRINTLNF("  cseg 16 len: %hu", tag_apm->cseg_16_len);
-            PRINTLNF("  dseg len: %hu",    tag_apm->dseg_len);
+            PRINTLNF("  version: %"KERNAUX_PRIu16,     tag_apm->version);
+            PRINTLNF("  cseg: %"KERNAUX_PRIu16,        tag_apm->cseg);
+            PRINTLNF("  offset: %"KERNAUX_PRIu32,      tag_apm->offset);
+            PRINTLNF("  cseg 16: %"KERNAUX_PRIu16,     tag_apm->cseg_16);
+            PRINTLNF("  dseg: %"KERNAUX_PRIu16,        tag_apm->dseg);
+            PRINTLNF("  flags: %"KERNAUX_PRIu16,       tag_apm->flags);
+            PRINTLNF("  cseg len: %"KERNAUX_PRIu16,    tag_apm->cseg_len);
+            PRINTLNF("  cseg 16 len: %"KERNAUX_PRIu16, tag_apm->cseg_16_len);
+            PRINTLNF("  dseg len: %"KERNAUX_PRIu16,    tag_apm->dseg_len);
         }
         break;
     case KERNAUX_MULTIBOOT2_ITAG_EFI_32BIT_SYSTEM_TABLE_PTR:
@@ -175,10 +173,19 @@ void KernAux_Multiboot2_ITagBase_print(
             const struct KernAux_Multiboot2_ITag_SMBIOSTables *const tag_smbios =
                 (struct KernAux_Multiboot2_ITag_SMBIOSTables*)tag_base;
 
-            PRINTLNF("  major: %u", tag_smbios->major);
-            PRINTLNF("  minor: %u", tag_smbios->minor);
+            PRINTLNF("  major: %"KERNAUX_PRIu8, tag_smbios->major);
+            PRINTLNF("  minor: %"KERNAUX_PRIu8, tag_smbios->minor);
 
-            PRINTLNF("  reserved1: {%u, %u, %u, %u, %u, %u}",
+            PRINTLNF(
+                "  reserved1: "
+                "{"
+                "%"KERNAUX_PRIu8", "
+                "%"KERNAUX_PRIu8", "
+                "%"KERNAUX_PRIu8", "
+                "%"KERNAUX_PRIu8", "
+                "%"KERNAUX_PRIu8", "
+                "%"KERNAUX_PRIu8
+                "}",
                 tag_smbios->reserved1[0],
                 tag_smbios->reserved1[1],
                 tag_smbios->reserved1[2],
@@ -221,7 +228,7 @@ void KernAux_Multiboot2_ITagBase_print(
         }
         break;
     case KERNAUX_MULTIBOOT2_ITAG_IMAGE_LOAD_BASE_PHYS_ADDR:
-        PRINTLNF("  load base addr: %u",
+        PRINTLNF("  load base addr: %"KERNAUX_PRIu32,
             ((struct KernAux_Multiboot2_ITag_ImageLoadBasePhysAddr*)tag_base)->
                 load_base_addr
         );
@@ -271,8 +278,8 @@ void KernAux_Multiboot2_ITag_MemoryMap_print(
         return;
     }
 
-    PRINTLNF("  entry size: %u",    tag->entry_size);
-    PRINTLNF("  entry version: %u", tag->entry_version);
+    PRINTLNF("  entry size: %"KERNAUX_PRIu32,    tag->entry_size);
+    PRINTLNF("  entry version: %"KERNAUX_PRIu32, tag->entry_version);
     PRINTLN("  entries:");
 
     const struct KernAux_Multiboot2_ITag_MemoryMap_EntryBase *const entries =
@@ -284,17 +291,12 @@ void KernAux_Multiboot2_ITag_MemoryMap_print(
         index < (tag->base.size - sizeof(*tag)) / tag->entry_size;
         ++index
     ) {
-        PRINTLNF(INT_IF("    entry %u", "    entry %lu"), index);
-        PRINTLNF(
-            INT_IF("      base addr: %llu", "      base addr: %lu"),
-            entries[index].base_addr
-        );
-        PRINTLNF(
-            INT_IF("      length: %llu", "      length: %lu"),
-            entries[index].length
-        );
-        PRINTLNF("      type: %u",        entries[index].type);
-        PRINTLNF("      reserved1: %u",   entries[index].reserved1);
+        // FIXME: size_t, not uint64_t
+        PRINTLNF("    entry %lu", index);
+        PRINTLNF("      base addr: %"KERNAUX_PRIu64, entries[index].base_addr);
+        PRINTLNF("      length: %"KERNAUX_PRIu64,    entries[index].length);
+        PRINTLNF("      type: %"KERNAUX_PRIu32,      entries[index].type);
+        PRINTLNF("      reserved1: %"KERNAUX_PRIu32, entries[index].reserved1);
     }
 }
 
@@ -310,10 +312,10 @@ void KernAux_Multiboot2_ITag_ELFSymbols_print(
         return;
     }
 
-    PRINTLNF("  num: %hu",       tag->num);
-    PRINTLNF("  entsize: %hu",   tag->ent_size);
-    PRINTLNF("  shndx: %hu",     tag->shndx);
-    PRINTLNF("  reserved1: %hu", tag->reserved1);
+    PRINTLNF("  num: %"KERNAUX_PRIu16,       tag->num);
+    PRINTLNF("  entsize: %"KERNAUX_PRIu16,   tag->ent_size);
+    PRINTLNF("  shndx: %"KERNAUX_PRIu16,     tag->shndx);
+    PRINTLNF("  reserved1: %"KERNAUX_PRIu16, tag->reserved1);
 
     // TODO: implement this
 }
