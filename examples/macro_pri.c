a4b4d556f0461c4bef63c590acd999c92154f33f
Alex Kotov <kotovalexarian@gmail.com>
2022-12-10 17:40:19 +0400

    Fix cppcheck

--- examples/macro_pri.c
+++ examples/macro_pri.c
@@ -21,18 +21,18 @@ void example_main()
     sprintf(buffer, "%"KERNAUX_PRIi16, (int16_t)32767);
     assert(strcmp(buffer, "32767") == 0);
 
+    // cppcheck-suppress invalidPrintfArgType_uint
     sprintf(buffer, "%"KERNAUX_PRIu32, (uint32_t)4294967295u);
     assert(strcmp(buffer, "4294967295") == 0);
 
+    // cppcheck-suppress invalidPrintfArgType_sint
     sprintf(buffer, "%"KERNAUX_PRIi32, (int32_t)2147483647);
     assert(strcmp(buffer, "2147483647") == 0);
 
-    // cppcheck doesn't understand this macro magic
     // cppcheck-suppress invalidPrintfArgType_uint
     sprintf(buffer, "%"KERNAUX_PRIu64, (uint64_t)18446744073709551615ull);
     assert(strcmp(buffer, "18446744073709551615") == 0);
 
-    // cppcheck doesn't understand this macro magic
     // cppcheck-suppress invalidPrintfArgType_sint
     sprintf(buffer, "%"KERNAUX_PRIi64, (int64_t)9223372036854775807ll);
     assert(strcmp(buffer, "9223372036854775807") == 0);
