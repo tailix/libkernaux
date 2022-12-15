110f553877d1ce5ba2af955fce0d0192d20fb107
Alex Kotov <kotovalexarian@gmail.com>
2022-12-15 14:11:13 +0400

    Improve code

--- src/multiboot2/info_convert.c
+++ src/multiboot2/info_convert.c
@@ -22,12 +22,16 @@ bool KernAux_Multiboot2_Info_to_memmap(
     KernAux_MemMap_init(memmap, 0);
     if (!KernAux_Multiboot2_Info_is_valid(multiboot2_info)) return false;
 
-    const struct KernAux_Multiboot2_ITagBase *const basic_memory_info_tag_base =
+    const struct KernAux_Multiboot2_ITag_BasicMemoryInfo *const
+        basic_memory_info_tag =
+        (const struct KernAux_Multiboot2_ITag_BasicMemoryInfo*)
         KernAux_Multiboot2_Info_first_tag_with_type(
             multiboot2_info,
             KERNAUX_MULTIBOOT2_ITAG_BASIC_MEMORY_INFO
         );
-    const struct KernAux_Multiboot2_ITagBase *const memory_map_tag_base =
+    const struct KernAux_Multiboot2_ITag_MemoryMap *const
+        memory_map_tag =
+        (const struct KernAux_Multiboot2_ITag_MemoryMap*)
         KernAux_Multiboot2_Info_first_tag_with_type(
             multiboot2_info,
             KERNAUX_MULTIBOOT2_ITAG_MEMORY_MAP
@@ -37,18 +41,9 @@ bool KernAux_Multiboot2_Info_to_memmap(
     // EFI platforms if EFI boot services are enabled and available for the
     // loaded image (EFI boot services not terminated tag exists in Multiboot2
     // information structure).
-    if (basic_memory_info_tag_base == NULL || memory_map_tag_base == NULL) {
+    if (basic_memory_info_tag == NULL || memory_map_tag == NULL) {
         return false;
     }
-
-    const struct KernAux_Multiboot2_ITag_BasicMemoryInfo *const
-        basic_memory_info_tag =
-        (const struct KernAux_Multiboot2_ITag_BasicMemoryInfo*)
-        basic_memory_info_tag_base;
-    const struct KernAux_Multiboot2_ITag_MemoryMap *const
-        memory_map_tag =
-        (const struct KernAux_Multiboot2_ITag_MemoryMap*)
-        memory_map_tag_base;
 
     // FIXME: The value returned for upper memory is maximally the address of
     // the first upper memory hole minus 1 megabyte. It is not guaranteed to be
