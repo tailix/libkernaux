be00fedcf9d2c5573199d81fb37db5c653f75da3
Alex Kotov <kotovalexarian@gmail.com>
2022-12-17 08:51:37 +0400

    Add more tests

--- bindings/ruby/spec/lib/kernaux/multiboot2/header_spec.rb
+++ bindings/ruby/spec/lib/kernaux/multiboot2/header_spec.rb
@@ -7,31 +7,83 @@ if KernAux::Version.with_multiboot2?
     subject(:multiboot2_header) { described_class.new data }
 
     let :data do
-      [magic_data, arch_data, "\x00" * 8].join.freeze
+      [
+        [Integer(magic_data)].pack('I'),
+        [Integer(arch_data)].pack('I'),
+        "\x00" * 8,
+      ].join.freeze
     end
 
-    let(:magic_data) { "\xd6\x50\x52\xe8" }
-    let(:arch_data)  { "\x04\x00\x00\x00" }
+    let(:magic_data) { described_class::MAGIC }
+    let(:arch_data)  { 0 }
 
     describe '#magic' do
       subject(:magic) { multiboot2_header.magic }
 
       it { is_expected.to be_instance_of Integer }
       it { is_expected.to equal described_class::MAGIC }
+
+      context 'when magic data is zero' do
+        let(:magic_data) { 0 }
+
+        it { is_expected.to be_instance_of Integer }
+        it { is_expected.to equal magic_data }
+      end
+
+      context 'when magic data is some other value' do
+        let(:magic_data) { 0x1234abcd }
+
+        it { is_expected.to be_instance_of Integer }
+        it { is_expected.to equal magic_data }
+      end
     end
 
     describe '#arch' do
       subject(:arch) { multiboot2_header.arch }
 
       it { is_expected.to be_instance_of Integer }
-      it { is_expected.to equal 4 }
+      it { is_expected.to equal arch_data }
+
+      context 'when arch data is zero' do
+        let(:arch_data) { 0 }
+
+        it { is_expected.to be_instance_of Integer }
+        it { is_expected.to equal arch_data }
+      end
+
+      context 'when arch data is some other value' do
+        let(:arch_data) { 0x1234abcd }
+
+        it { is_expected.to be_instance_of Integer }
+        it { is_expected.to equal arch_data }
+      end
     end
 
     describe '#arch_name' do
       subject(:arch_name) { multiboot2_header.arch_name }
 
       it { is_expected.to be_instance_of Symbol }
-      it { is_expected.to equal :mips32 }
+      it { is_expected.to equal described_class::ARCH.fetch arch_data }
+
+      context 'when arch is i386' do
+        let(:arch_data) { 0 }
+
+        it { is_expected.to be_instance_of Symbol }
+        it { is_expected.to equal :i386 }
+      end
+
+      context 'when arch is MIPS32' do
+        let(:arch_data) { 4 }
+
+        it { is_expected.to be_instance_of Symbol }
+        it { is_expected.to equal :mips32 }
+      end
+
+      context 'when arch data is some other value' do
+        let(:arch_data) { 0x1234abcd }
+
+        it { is_expected.to equal nil }
+      end
     end
   end
 end
