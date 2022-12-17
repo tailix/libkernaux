80eb8906529cc1ba263896804e1a2adc327967ce
Alex Kotov <kotovalexarian@gmail.com>
2022-12-17 08:09:08 +0400

    Add method KernAux::Multiboot2::Header#magic

--- bindings/ruby/ext/default/multiboot2.c
+++ bindings/ruby/ext/default/multiboot2.c
@@ -1,13 +1,74 @@
 #include "main.h"
 
+#include <stddef.h>
+
 #ifdef KERNAUX_VERSION_WITH_MULTIBOOT2
 
-VALUE rb_KernAux_Multiboot2 = Qnil;
+#define RBNSMDL rb_KernAux_Multiboot2
+#define RBNS(s) rb_KernAux_Multiboot2_##s
+
+#define ENSURE_BASE_SIZE(data, Type) do { \
+    const long len = RSTRING_LEN(data);                                       \
+    if (len < 0 || (size_t)len <= sizeof(struct KernAux_Multiboot2_##Type)) { \
+        return Qnil;                                                          \
+    }                                                                         \
+} while (0)
+
+VALUE RBNSMDL             = Qnil; // KernAux::Multiboot2
+VALUE RBNS(BaseSizeError) = Qnil; // KernAux::Multiboot2::BaseSizeError
+VALUE RBNS(InvalidError)  = Qnil; // KernAux::Multiboot2::InvalidError
+VALUE RBNS(Struct)        = Qnil; // KernAux::Multiboot2::Struct
+VALUE RBNS(Header)        = Qnil; // KernAux::Multiboot2::Header
+
+/**
+ * Return the magic field of the Multiboot 2 header.
+ *
+ * @return [nil, Integer]
+ *
+ * @see https://www.gnu.org/software/grub/manual/multiboot2/multiboot.html#Header-magic-fields
+ */
+// KernAux::Multiboot2::Header#magic
+static VALUE rb_KernAux_Multiboot2_Header_magic(VALUE self);
 
 void init_multiboot2()
 {
+    // KernAux::Multiboot2
     rb_gc_register_mark_object(rb_KernAux_Multiboot2 =
         rb_define_module_under(rb_KernAux, "Multiboot2"));
+
+    // KernAux::Multiboot2::BaseSizeError
+    rb_gc_register_mark_object(rb_KernAux_Multiboot2_BaseSizeError =
+        rb_define_class_under(
+            rb_KernAux_Multiboot2, "BaseSizeError", rb_KernAux_Error));
+
+    // KernAux::Multiboot2::InvalidError
+    rb_gc_register_mark_object(rb_KernAux_Multiboot2_InvalidError =
+        rb_define_class_under(
+            rb_KernAux_Multiboot2, "InvalidError", rb_KernAux_Error));
+
+    // KernAux::Multiboot2::Struct
+    rb_gc_register_mark_object(rb_KernAux_Multiboot2_Struct =
+        rb_define_class_under(
+            rb_KernAux_Multiboot2, "Struct", rb_cObject));
+
+    // KernAux::Multiboot2::Header
+    rb_gc_register_mark_object(rb_KernAux_Multiboot2_Header =
+        rb_define_class_under(
+            rb_KernAux_Multiboot2, "Header", rb_KernAux_Multiboot2_Struct));
+    rb_define_method(rb_KernAux_Multiboot2_Header, "magic",
+                     rb_KernAux_Multiboot2_Header_magic, 0);
+}
+
+// KernAux::Multiboot2::Header#magic
+VALUE rb_KernAux_Multiboot2_Header_magic(VALUE self)
+{
+    VALUE data = rb_ivar_get(self, rb_intern("@data"));
+    const struct KernAux_Multiboot2_Header *const multiboot2_header =
+        (const struct KernAux_Multiboot2_Header*)
+        StringValuePtr(data);
+    ENSURE_BASE_SIZE(data, Header);
+    KERNAUX_CAST_CONST(unsigned long, magic, multiboot2_header->magic);
+    return ULONG2NUM(magic);
 }
 
 #endif
--- bindings/ruby/lib/kernaux/multiboot2.rb
+++ bindings/ruby/lib/kernaux/multiboot2.rb
@@ -7,5 +7,81 @@ module KernAux
   # @see https://www.gnu.org/software/grub/manual/multiboot2/multiboot.html
   #
   module Multiboot2
+    ##
+    # Raised when trying to create a Multiboot 2 structure
+    # which is shorter than even it's base.
+    #
+    # @see Struct#enough!
+    #
+    class BaseSizeError < Error; end
+
+    ##
+    # Raised when an invalid Multiboot 2 structure requested to be valid.
+    #
+    # @see Struct#valid!
+    #
+    class InvalidError < Error; end
+
+    ##
+    # The base class for any Multiboot 2 structure.
+    #
+    class Struct
+      def initialize(data)
+        @data = String data
+      end
+
+      ##
+      # Freeze the structure and it's underlying data.
+      #
+      def freeze
+        @data.freeze
+        super
+        nil
+      end
+
+      def enough?
+        raise NotImplementedError, "#{self.class}#enough?"
+      end
+
+      def valid?
+        raise NotImplementedError, "#{self.class}#valid?"
+      end
+
+      ##
+      # @raise [BaseSizeError] the structure size is too small
+      #
+      def enough!
+        raise BaseSizeError, 'The structure size is too small' unless enough?
+      end
+
+      ##
+      # @raise [BaseSizeError] the structure size is too small
+      # @raise [InvalidError] the structure is invalid
+      #
+      def valid!
+        enough!
+        raise InvalidError, 'The structure is invalid' unless valid?
+      end
+    end
+
+    ##
+    # Multiboot 2 header.
+    #
+    # @see https://www.gnu.org/software/grub/manual/multiboot2/multiboot.html#Header-layout
+    #
+    class Header < Struct
+      ##
+      # Header magic number.
+      #
+      # @see https://www.gnu.org/software/grub/manual/multiboot2/multiboot.html#Header-magic-fields
+      #
+      MAGIC = 0xe85250d6
+
+      def initialize(data)
+        super
+        # TODO: use some const
+        raise BaseSizeError, 'Too short base' unless @data.bytesize < 16
+      end
+    end
   end
 end
