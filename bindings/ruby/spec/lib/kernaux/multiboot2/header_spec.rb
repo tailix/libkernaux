8cd09497c232b958bc2aa569c4a7160d841a26a2
Alex Kotov <kotovalexarian@gmail.com>
2022-12-17 17:51:50 +0400

    Use fixtures in specs

--- bindings/ruby/spec/lib/kernaux/multiboot2/header_spec.rb
+++ bindings/ruby/spec/lib/kernaux/multiboot2/header_spec.rb
@@ -4,109 +4,45 @@ require 'spec_helper'
 
 if KernAux::Version.with_multiboot2?
   RSpec.describe KernAux::Multiboot2::Header do
-    subject(:multiboot2_header) { described_class.new data }
+    subject(:multiboot2_header) { described_class.new fixture_data }
 
-    let :data do
-      [
-        [Integer(magic_data)].pack('I'),
-        [Integer(arch_data)].pack('I'),
-        [Integer(total_size_data)].pack('I'),
-        "\x00" * 4,
-      ].join.freeze
+    let(:fixture) { 0 }
+
+    let(:fixture_data) { File.read fixture_path }
+
+    let :fixture_path do
+      File.expand_path(
+        "../../../../../../fixtures/multiboot2_header_example#{fixture}.bin",
+        __dir__
+      )
     end
-
-    let(:magic_data) { described_class::MAGIC }
-    let(:arch_data) { 0 }
-    let(:total_size_data) { 20 }
 
     describe '#magic' do
       subject(:magic) { multiboot2_header.magic }
 
       it { is_expected.to be_instance_of Integer }
       it { is_expected.to equal described_class::MAGIC }
-
-      context 'when magic data is zero' do
-        let(:magic_data) { 0 }
-
-        it { is_expected.to be_instance_of Integer }
-        it { is_expected.to equal magic_data }
-      end
-
-      context 'when magic data is some other value' do
-        let(:magic_data) { 0x1234abcd }
-
-        it { is_expected.to be_instance_of Integer }
-        it { is_expected.to equal magic_data }
-      end
     end
 
     describe '#arch' do
       subject(:arch) { multiboot2_header.arch }
 
       it { is_expected.to be_instance_of Integer }
-      it { is_expected.to equal arch_data }
-
-      context 'when arch data is zero' do
-        let(:arch_data) { 0 }
-
-        it { is_expected.to be_instance_of Integer }
-        it { is_expected.to equal arch_data }
-      end
-
-      context 'when arch data is some other value' do
-        let(:arch_data) { 0x1234abcd }
-
-        it { is_expected.to be_instance_of Integer }
-        it { is_expected.to equal arch_data }
-      end
+      it { is_expected.to equal 0 }
     end
 
     describe '#arch_name' do
       subject(:arch_name) { multiboot2_header.arch_name }
 
       it { is_expected.to be_instance_of Symbol }
-      it { is_expected.to equal described_class::ARCH.fetch arch_data }
-
-      context 'when arch is i386' do
-        let(:arch_data) { 0 }
-
-        it { is_expected.to be_instance_of Symbol }
-        it { is_expected.to equal :i386 }
-      end
-
-      context 'when arch is MIPS32' do
-        let(:arch_data) { 4 }
-
-        it { is_expected.to be_instance_of Symbol }
-        it { is_expected.to equal :mips32 }
-      end
-
-      context 'when arch data is some other value' do
-        let(:arch_data) { 0x1234abcd }
-
-        it { is_expected.to equal nil }
-      end
+      it { is_expected.to equal :i386 }
     end
 
     describe '#total_size' do
       subject(:total_size) { multiboot2_header.total_size }
 
       it { is_expected.to be_instance_of Integer }
-      it { is_expected.to equal total_size_data }
-
-      context 'when total size data is zero' do
-        let(:total_size_data) { 0 }
-
-        it { is_expected.to be_instance_of Integer }
-        it { is_expected.to equal total_size_data }
-      end
-
-      context 'when total size data is some other value' do
-        let(:total_size_data) { 0x1234abcd }
-
-        it { is_expected.to be_instance_of Integer }
-        it { is_expected.to equal total_size_data }
-      end
+      it { is_expected.to equal 24 }
     end
   end
 end
