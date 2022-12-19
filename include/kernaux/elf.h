b76139212e9dda7e52a21e54d2cc9de3c32c2662
Alex Kotov <kotovalexarian@gmail.com>
2022-12-19 13:33:31 +0400

    Use uint*_t types

--- include/kernaux/elf.h
+++ include/kernaux/elf.h
@@ -8,71 +8,72 @@ extern "C" {
 #include <kernaux/macro.h>
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #include <kernaux/macro/packing_start.run>
 
 struct KernAux_ELF_Header {
-    unsigned magic_0x7f     : 8;
-    unsigned magic_E        : 8;
-    unsigned magic_L        : 8;
-    unsigned magic_F        : 8;
-    unsigned bitness        : 8;
-    unsigned endianness     : 8;
-    unsigned header_version : 8;
-    unsigned os_abi         : 8;
-    unsigned unused1        : 32;
-    unsigned unused2        : 32;
-    unsigned obj_type       : 16;
-    unsigned isa            : 16;
-    unsigned elf_version    : 32;
-    unsigned entrypoint     : 32;
-    unsigned prog_table_pos : 32;
-    unsigned sect_table_pos : 32;
-    unsigned arch_flags     : 32;
-    unsigned header_size    : 16;
-    unsigned prog_entr_size : 16;
-    unsigned prog_entr_num  : 16;
-    unsigned sect_entr_size : 16;
-    unsigned sect_entr_num  : 16;
-    unsigned sect_names_idx : 16;
+    uint8_t magic_0x7f;
+    uint8_t magic_E;
+    uint8_t magic_L;
+    uint8_t magic_F;
+    uint8_t bitness;
+    uint8_t endianness;
+    uint8_t header_version;
+    uint8_t os_abi;
+    uint32_t unused1;
+    uint32_t unused2;
+    uint16_t obj_type;
+    uint16_t isa;
+    uint32_t elf_version;
+    uint32_t entrypoint;
+    uint32_t prog_table_pos;
+    uint32_t sect_table_pos;
+    uint32_t arch_flags;
+    uint16_t header_size;
+    uint16_t prog_entr_size;
+    uint16_t prog_entr_num;
+    uint16_t sect_entr_size;
+    uint16_t sect_entr_num;
+    uint16_t sect_names_idx;
 }
 KERNAUX_PACKED;
 
 KERNAUX_STATIC_TEST_STRUCT_SIZE(KernAux_ELF_Header, 52);
 
 struct KernAux_ELF_ProgramEntry {
-    unsigned type      : 32;
-    unsigned offset    : 32;
-    unsigned virt_addr : 32;
-    unsigned phys_addr : 32;
-    unsigned file_size : 32;
-    unsigned mem_size  : 32;
-    unsigned flags     : 32;
-    unsigned align     : 32;
+    uint32_t type;
+    uint32_t offset;
+    uint32_t virt_addr;
+    uint32_t phys_addr;
+    uint32_t file_size;
+    uint32_t mem_size;
+    uint32_t flags;
+    uint32_t align;
 }
 KERNAUX_PACKED;
 
 KERNAUX_STATIC_TEST_STRUCT_SIZE(KernAux_ELF_ProgramEntry, 32);
 
 struct KernAux_ELF_SectionEntry {
-    unsigned name        : 32;
-    unsigned type        : 32;
-    unsigned flags       : 32;
-    unsigned vaddr       : 32;
-    unsigned file_offset : 32;
-    unsigned file_size   : 32;
-    unsigned link        : 32;
-    unsigned info        : 32;
-    unsigned alignment   : 32;
-    unsigned ent_size    : 32;
+    uint32_t name;
+    uint32_t type;
+    uint32_t flags;
+    uint32_t vaddr;
+    uint32_t file_offset;
+    uint32_t file_size;
+    uint32_t link;
+    uint32_t info;
+    uint32_t alignment;
+    uint32_t ent_size;
 }
 KERNAUX_PACKED;
 
 KERNAUX_STATIC_TEST_STRUCT_SIZE(KernAux_ELF_SectionEntry, 40);
 
 struct KernAux_ELF_RelocationEntry {
-    unsigned virt_addr : 32;
-    unsigned info      : 32;
+    uint32_t virt_addr;
+    uint32_t info;
 }
 KERNAUX_PACKED;
 
