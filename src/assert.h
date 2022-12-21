1348e7edebf2d994d3fa4adb4ec779835b9922e2
Alex Kotov <kotovalexarian@gmail.com>
2022-12-21 12:25:11 +0400

    Remove unnecessary definition

--- src/assert.h
+++ src/assert.h
@@ -3,12 +3,12 @@
 
 #include <kernaux/runtime.h>
 
-#ifdef KERNAUX_DISABLE_ASSERT
-#define KERNAUX_PANIC(msg) ((void)0)
-#define KERNAUX_ASSERT(cond) ((void)0)
-#else
+#ifdef ENABLE_ASSERT
 #define KERNAUX_PANIC(msg) (kernaux_assert_do(__FILE__, __LINE__, msg))
 #define KERNAUX_ASSERT(cond) ((cond) ? (void)0 : KERNAUX_PANIC(#cond))
+#else
+#define KERNAUX_PANIC(msg) ((void)0)
+#define KERNAUX_ASSERT(cond) ((void)0)
 #endif
 
 #define KERNAUX_NOTNULL(cond) KERNAUX_ASSERT(cond)
